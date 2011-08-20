442e5c2aa26761fe08944cf619ee282deda21f88
M. Elizabeth Scott <beth@scanlime.org>
2011-08-20 14:52:04 -0700

    Add notes on flash memory model & power usage

--- src/flash.c
+++ src/flash.c
@@ -6,6 +6,28 @@
  * Copyright <c> 2011 Sifteo, Inc. All rights reserved.
  */
 
+/*
+ * XXX: Pick a specific flash memory model.
+ * 
+ * I've had my eye on the Numonyx/Micron M29W160ET70N6E.
+ * It has a number of nice features:
+ *
+ *  - Less than $1 on Digi-key, in qty 100k
+ *
+ *  - 16 megabits, which is our maximum size using this 21-bit addressing technique
+ *
+ *  - 8-bit parallel interface, with random-access read
+ *
+ *  - Automatic standby. It will draw 4.5mA when "actively" reading, however
+ *    it automatically enters a sub-100uA standby mode if the bus is idle for 150 nS.
+ *    Our maximum read rate is (12 MHz / 3 cycles per inc / 2 incs per read) 2 MHz,
+ *    which is a period of 500nS. So even if we're spending 100% of our CPU time
+ *    reading the flash, it should draw an average of (150nS active / 500nS period *
+ *    4.5mA full power + 0.1mA standby) 1.45 mA. Not too bad. And in practice, we'll
+ *    be spending CPU time doing plenty of other things. We could integrate rough
+ *    power usage monitoring into the simulation perhaps.
+ */
+
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -26,6 +48,9 @@ void flash_init(const char *filename)
     size_t result;
 
     flash.file = fopen(filename, "rb+");
+    if (!flash.file) {
+        flash.file = fopen(filename, "wb+");
+    }
     if (!flash.file) {
 	perror("Error opening flash");
 	exit(1);
