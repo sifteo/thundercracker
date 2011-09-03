ed6760ee05cd98f8cef594612716bf089b18b108
M. Elizabeth Scott <beth@sifteo.com>
2011-09-02 17:31:25 -0700

    More notes on flash requirements

--- emulator/cube/flash.c
+++ emulator/cube/flash.c
@@ -8,24 +8,26 @@
 
 /*
  * XXX: Pick a specific flash memory model.
- * 
- * I've had my eye on the Numonyx/Micron M29W160ET70N6E.
- * It has a number of nice features:
  *
- *  - Less than $1 on Digi-key, in qty 100k
+ * Currently this is based on the SST39VF1681, but this likely isn't
+ * what we'll actually be using in the final design.
  *
- *  - 16 megabits, which is our maximum size using this 21-bit addressing technique
+ * Our current requirements:
  *
- *  - 8-bit parallel interface, with random-access read
+ *  - NOR flash, 2Mx8 (16 megabits)
  *
- *  - Automatic standby. It will draw 4.5mA when "actively" reading, however
- *    it automatically enters a sub-100uA standby mode if the bus is idle for 150 nS.
- *    Our maximum read rate is (12 MHz / 3 cycles per inc / 2 incs per read) 2 MHz,
- *    which is a period of 500nS. So even if we're spending 100% of our CPU time
- *    reading the flash, it should draw an average of (150nS active / 500nS period *
- *    4.5mA full power + 0.1mA standby) 1.45 mA. Not too bad. And in practice, we'll
- *    be spending CPU time doing plenty of other things. We could integrate rough
- *    power usage monitoring into the simulation perhaps.
+ *  - Low power (Many memories have an automatic standby feature, which gives them
+ *    fairly low power usage when we're clocking them slowly. Our actual clock rate
+ *    will be 2.67 MHz peak.)
+ *
+ *  - Fast program times. The faster we can get the better, this will directly
+ *    impact asset download times.
+ *
+ *  - Fast erase times. This is very important- we'd like to stream assets in the
+ *    background, but during an erase we can't refresh the screen at all until the
+ *    erase finishes. This is clearly very bad for interactivity, so flashes with
+ *    erase times in the tens of milliseconds are much better than flashes that
+ *    take half a second or longer to erase.
  */
 
 #include <stdint.h>
