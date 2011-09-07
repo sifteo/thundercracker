c44556f6492c379b4eca70fc2b42a254496b2a86
M. Elizabeth Scott <beth@sifteo.com>
2011-09-07 15:45:48 -0700

    Flash decoder: callee_saves pragma
    This is a modest performance boost... but mostly it makes me
    want to rewrite big pieces of this in assembly. Bah!

--- firmware/cube/flash.h
+++ firmware/cube/flash.h
@@ -22,6 +22,8 @@
 volatile extern uint8_t __idata flash_fifo[FLASH_FIFO_MASK + 1];
 volatile extern uint8_t flash_fifo_head;
 
+#pragma callee_saves flash_wait, flash_erase, flash_program
+
 void flash_init(void);
 void flash_handle_fifo();
 void flash_wait(void);
