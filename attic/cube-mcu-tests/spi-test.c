4d5c444cce67554e67adfe1be2c6bcc5fcae261d
M. Elizabeth Scott <beth@sifteo.com>
2011-09-08 17:10:30 -0700

    Add a simple SPI test, to verify the 2-level FIFO on real hardware.

---
+++ attic/cube-mcu-tests/spi-test.c
@@ -0,0 +1,33 @@
+/* -*- mode: C; c-basic-offset: 4; intent-tabs-mode: nil -*-
+ *
+ * SPI test for the nrF24LE1.
+ *
+ * The Nordic datasheet was vague about the SPI timing and FIFOs..
+ * This will give me a better idea of how they *actually* perform.
+ *
+ * M. Elizabeth Scott <beth@sifteo.com>
+ * Copyright <c> 2011 Sifteo, Inc. All rights reserved.
+ */
+
+#include <stdint.h>
+#include "hardware.h"
+
+void main()
+{
+    uint8_t i = 0; 
+
+    P0DIR = 0x00;
+    P0 = 0xC0;
+
+    SPIMCON1 = 0x0F;
+    SPIMCON0 = 0x01;
+
+    // Keep FIFO full
+    SPIMDAT = ++i;
+    while (1) {
+	P0 ^= 1;
+	SPIMDAT = ++i;
+	while (!(SPIMSTAT & SPI_RX_READY));
+	SPIMDAT;
+    }
+}
