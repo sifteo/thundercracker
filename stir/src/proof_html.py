7c6e3db5ee4ab9b57174ae6e991bc94b2a4d4cfe
M. Elizabeth Scott <beth@sifteo.com>
2011-09-13 14:27:50 -0700

    Proofer: Hilight tiles interactively

--- stir/src/proof_html.py
+++ stir/src/proof_html.py
@@ -20,9 +20,16 @@ header = """
 
      function defineTiles(prefix, tiles) {
        for (var i = 0; i < tiles.length; i++) {
+
+         // Load it using a Data URI and anonymous img element
          var img = new Image(8, 8);
          img.src = prefix + tiles[i];
-         tiles[i] = img;
+
+         // Keep a list of closures that can be used to redraw
+         // every occurrance of this tile in the TileGrids.
+         img.tgRedraw = []
+ 
+        tiles[i] = img;
        }
        return tiles;
      }
@@ -32,50 +39,178 @@ header = """
       * Renders onto an HTML5 Canvas
       */
 
-     allTileGrids = []
+     allTileGrids = [];
+     highlightTile = null;
 
      function TileGrid(pool, canvasId) {
-         allTileGrids.push(this);
-         this.pool = pool;
-         this.canvas = document.getElementById(canvasId);
-         this.ctx = this.canvas.getContext("2d");
-         this.width = this.canvas.width / 8;
-         this.height = this.canvas.height / 8;
+       var obj = this;
+
+       allTileGrids.push(this);
+
+       this.pool = pool;
+       this.canvas = document.getElementById(canvasId);
+       this.ctx = this.canvas.getContext("2d");
+       this.width = this.canvas.width / 8;
+       this.height = this.canvas.height / 8;
+
+       this.canvas.onmousemove = function(evt) {
+         setTileHighlight(obj.tileAt(Math.floor(mouseX(evt) / 8),
+                                     Math.floor(mouseY(evt) / 8)));
+       }
+
+       this.canvas.onmouseout = function(evt) {
+         setTileHighlight(null);
+       }
      }
 
      TileGrid.prototype.range = function(begin, end) {
-         this.tiles = [];
-         for (var i = begin; i < end; i++)
-             this.tiles[i - begin] = i;
+       var a = [];
+       for (var i = begin; i < end; i++)
+         a[i - begin] = i;
+       this.array(a);
      }
 
      TileGrid.prototype.array = function(a) {
-         this.tiles = a;
+       this.tiles = a;
      }
 
      TileGrid.prototype.draw = function() {
-       var i = 0;
+       /*
+        * Draw the whole TileGrid unconditionally
+        */
 
        for (var y = 0; y < this.height; y++)
-         for (var x = 0; x < this.width; x++) {
+         for (var x = 0; x < this.width; x++)
+           this.drawTile(x, y);
+     }
 
-           this.ctx.drawImage(this.pool[this.tiles[i]], x*8, y*8);
+     TileGrid.prototype.installDrawHandlers = function() {
+       /*
+        * Install tile redraw handlers for every tile in the TileGrid
+        */
 
-           i++;
-           if (i >= this.tiles.length)
-             return;
+       for (var y = 0; y < this.height; y++)
+         for (var x = 0; x < this.width; x++)
+           var f = function (obj, x, y) {
+             var t = obj.tileAt(x, y);
+
+             if (t) {
+               t.tgRedraw.push(function() { obj.drawTile(x, y); })
+             }
+
+           } (this, x, y);
+     }
+
+     TileGrid.prototype.tileAt = function(x, y) {
+       /*
+        * Returns the tile at location (x,y), if any.
+        */
+
+       var i = x + y * this.width;
+       if (i < this.tiles.length)
+         return this.pool[this.tiles[i]];
+     }
+
+     TileGrid.prototype.drawTile = function(x, y) {
+       /*
+        * (Re)draw a single tile at a specified location on this grid.
+        */
+
+       var tile = this.tileAt(x, y);
+       if (tile) {
+
+         this.ctx.globalAlpha = 1.0;
+         this.ctx.drawImage(tile, x*8, y*8);
+
+         if (highlightTile != null && tile != highlightTile) {
+           // Dim this tile
+           this.ctx.globalAlpha = 0.5;
+           this.ctx.fillStyle = "#000";
+           this.ctx.fillRect(x*8, y*8, 8, 8);
          }
-     }
 
-     /*
-      * Draw all TileGrids after our images have loaded
-      */
-
-     function drawAllTileGrids() {
-       for (var i in allTileGrids)
-         allTileGrids[i].draw();
-     }
+       } else {
  
+         // Background
+         this.ctx.globalAlpha = 1.0;
+         this.ctx.fillStyle = "#000";
+         this.ctx.fillRect(x*8, y*8, 8, 8);
+       }
+     }
+
+     function onload() {
+       /*
+        * Draw all TileGrids after our images have loaded
+        */
+
+       for (var i in allTileGrids) {
+         allTileGrids[i].installDrawHandlers();
+         allTileGrids[i].draw();
+       }
+     }
+
+     function drawTile(t) {
+       /*
+        * Redraw a single tile, on every grid.
+        */
+
+       if (t) {
+         for (var i in t.tgRedraw)
+           t.tgRedraw[i]();
+       }
+     }
+
+     function setTileHighlight(t) {
+       /*
+        * Change the tile that's currently highlighted.
+        */
+
+       if (t != highlightTile) {
+         var prev = highlightTile;
+         highlightTile = t;
+
+         if (prev == null || t == null) {
+           /*
+            * Transitions from highlighted to not highlighted or vice versa
+            * require a full draw, since we're dimming all non-hilighted tiles.
+            */
+          
+           for (var i in allTileGrids)
+             allTileGrids[i].draw();
+
+         } else {
+           /*
+            * Only draw the affected tiles
+            */
+
+           drawTile(prev);
+           drawTile(t);
+         }
+       }
+     }
+
+     function mouseX(evt) {
+       var x;
+
+       if (evt.pageX)
+         x = evt.pageX;
+       else
+         x = evt.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+
+       return x - evt.target.offsetLeft;
+     }
+
+     function mouseY(evt) {
+       var y;
+
+       if (evt.pageY)
+         y = evt.pageY;
+       else
+         y = evt.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+
+       return y - evt.target.offsetTop;
+     }
+
   </script> 
   <style> 
  
@@ -115,7 +250,7 @@ header = """
  
   </style> 
 </head> 
-<body onload="drawAllTileGrids()"> 
+<body onload="onload()">
 """
 
 cppTemplate = """
