6348f92d08a3b9bd1d8ea925feaf75d6845bc07e
M. Elizabeth Scott <beth@sifteo.com>
2011-09-17 13:25:47 -0700

    Make the 'hello' demo try to say Hello World

--- sdk/examples/hello/main.cpp
+++ sdk/examples/hello/main.cpp
@@ -19,18 +19,36 @@ void vPoke(uint16_t addr, uint16_t word)
     Atomic::Or(vbuf.cm32, 0x80000000 >> (addr >> 5));
 }
 
+void vPokeIndex(uint16_t addr, uint16_t tile)
+{
+    printf("Poke %04x %04x\n", addr, tile);
+    vPoke(addr, ((tile << 1) & 0xFE) | ((tile << 2) & 0xFE00));
+}
+
+void putchar(uint8_t x, uint8_t y, char c)
+{
+    const uint8_t pitch = 20;
+    uint16_t index = (c - ' ') << 1;
+
+    vPokeIndex(x + pitch*y, index);
+    vPokeIndex(x + pitch*(y+1), index+1);
+}
+
+void print(uint8_t x, uint8_t y, const char *str)
+{
+    while (*str)
+	putchar(x++, y, *(str++));
+}
+
 void siftmain()
 {
     _SYS_enableCubes(1 << 0);
     _SYS_loadAssets(0, &GameAssets.sys);
     _SYS_setVideoBuffer(0, &vbuf);
 
-    uint16_t x = 0;
-
+    print(0, 0, "Hello World!");
+	
     while (1) {
-	x++;
-	vPoke(x & 511, x);
-
 	System::draw();
     }
 }
