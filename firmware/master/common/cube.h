0a6a49be0629c19c9647f45d60e13ccf09f4d742
M. Elizabeth Scott <beth@sifteo.com>
2011-10-07 22:08:01 -0700

    Retry flash resets if they don't make it
    We could lose a flash reset for any number of reasons.
    Give them a finite timeout.

--- firmware/master/common/cube.h
+++ firmware/master/common/cube.h
@@ -126,6 +126,9 @@ class CubeSlot {
     static void finishCubes(_SYSCubeIDVector cv);
 
  private:
+    // Limit on round-trip time
+    static const unsigned RTT_DEADLINE_MS = 250;
+    
     /*
      * Data buffers, provided by game code.
      *
@@ -145,7 +148,8 @@ class CubeSlot {
     _SYSAssetGroup *loadGroup;
     _SYSVideoBuffer *vbuf;
 
-    SysTime::Ticks paintTimestamp;
+    SysTime::Ticks paintTimestamp;      // Used only by thread
+    SysTime::Ticks flashDeadline;       // Used only by ISR
     int32_t pendingFrames;
 
     // Packet encoder state
