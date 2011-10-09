86019f58c61eab87faec21b109c43fe445b3cfca
M. Elizabeth Scott <beth@sifteo.com>
2011-10-08 19:53:57 -0700

    OpenGL texture clamping
    fixes some very subtle edge artifacts when zoomed in.

--- emulator/src/frontend_cube.cpp
+++ emulator/src/frontend_cube.cpp
@@ -106,6 +106,8 @@ void FrontendCube::initGL()
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
 }
 
  void FrontendCube::draw()
