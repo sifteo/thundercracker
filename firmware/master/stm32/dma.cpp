dc00051d4c9ea911b10f07d4796807bb9a3ecaab
Liam Staskawicz <liam@sifteo.com>
2011-10-11 11:39:31 -0700

    * centralized manager for DMA

---
+++ firmware/master/stm32/dma.cpp
@@ -0,0 +1,137 @@
+
+#include "dma.h"
+
+/*
+ * Several peripherals can be served by a single DMA channel, so this module
+ * provides a centralized way to manage which peripherals are signed up
+ * for particular DMA channels, and ensure only one is registered at a time.
+ */
+
+uint32_t Dma::Ch1Mask = 0;
+uint32_t Dma::Ch2Mask = 0;
+struct Dma::DmaHandler_t Dma::Ch1Handlers[7];
+struct Dma::DmaHandler_t Dma::Ch2Handlers[5];
+
+void Dma::registerHandler(volatile DMA_t *dma, int channel, DmaIsr_t func, void *param)
+{
+    if (dma == &DMA1) {
+        if (Ch1Mask & (1 << channel)) {
+            // already registered :(
+            return;
+        }
+        if (Ch1Mask == 0) {
+            RCC.AHBENR |= (1 << 0);
+            dma->IFCR = 0x0FFFFFFF; // clear all ISRs
+        }
+        Ch1Mask |= (1 << channel); // mark it as enabled
+        Ch1Handlers[channel].isrfunc = func;
+        Ch1Handlers[channel].param = param;
+    }
+    else if (dma == &DMA2) {
+        if (Ch2Mask & (1 << channel)) {
+            // already registered :(
+            return;
+        }
+        if (Ch1Mask == 0) {
+            RCC.AHBENR |= (1 << 1);
+            dma->IFCR = 0x0FFFFFFF; // clear all ISRs
+        }
+        Ch2Mask |= (1 << channel); // mark it as enabled
+        Ch2Handlers[channel].isrfunc = func;
+        Ch2Handlers[channel].param = param;
+    }
+}
+
+void Dma::unregisterHandler(volatile DMA_t *dma, int channel)
+{
+    if (dma == &DMA1) {
+        Ch1Mask &= ~(1 << channel); // mark it as disabled
+        if (Ch1Mask == 0) { // last one? shut it down
+            RCC.AHBENR &= ~(1 << 0);
+        }
+        Ch1Handlers[channel].isrfunc = 0;
+        Ch1Handlers[channel].param = 0;
+    }
+    else if (dma == &DMA2) {
+        Ch2Mask &= ~(1 << channel); // mark it as disabled
+        if (Ch2Mask == 0) { // last one? shut it down
+            RCC.AHBENR &= ~(1 << 1);
+        }
+        Ch2Handlers[channel].isrfunc = 0;
+        Ch2Handlers[channel].param = 0;
+    }
+}
+
+void Dma::serveIsr(volatile DMA_t *dma, int ch, struct DmaHandler_t *handlers)
+{
+    uint32_t flags = dma->ISR >> (ch * 4);
+    dma->IFCR = 1 << (ch * 4); // clear the channel
+    struct Dma::DmaHandler_t* handler = &handlers[ch];
+    if (handler->isrfunc) {
+        handler->isrfunc(handler->param, flags);
+    }
+}
+
+/*
+ * DMA1
+ */
+IRQ_HANDLER ISR_DMA1_Channel1()
+{
+    Dma::serveIsr(&DMA1, 0, Dma::Ch1Handlers);
+}
+
+IRQ_HANDLER ISR_DMA1_Channel2()
+{
+    Dma::serveIsr(&DMA1, 1, Dma::Ch1Handlers);
+}
+
+IRQ_HANDLER ISR_DMA1_Channel3()
+{
+    Dma::serveIsr(&DMA1, 2, Dma::Ch1Handlers);
+}
+
+IRQ_HANDLER ISR_DMA1_Channel4()
+{
+    Dma::serveIsr(&DMA1, 3, Dma::Ch1Handlers);
+}
+
+IRQ_HANDLER ISR_DMA1_Channel5()
+{
+    Dma::serveIsr(&DMA1, 4, Dma::Ch1Handlers);
+}
+
+IRQ_HANDLER ISR_DMA1_Channel6()
+{
+    Dma::serveIsr(&DMA1, 5, Dma::Ch1Handlers);
+}
+
+IRQ_HANDLER ISR_DMA1_Channel7()
+{
+    Dma::serveIsr(&DMA1, 6, Dma::Ch1Handlers);
+}
+
+/*
+ * DMA2
+ */
+
+IRQ_HANDLER ISR_DMA2_Channel1()
+{
+    Dma::serveIsr(&DMA2, 0, Dma::Ch2Handlers);
+}
+
+IRQ_HANDLER ISR_DMA2_Channel2()
+{
+    Dma::serveIsr(&DMA2, 1, Dma::Ch2Handlers);
+}
+
+IRQ_HANDLER ISR_DMA2_Channel3()
+{
+    Dma::serveIsr(&DMA2, 2, Dma::Ch2Handlers);
+}
+
+IRQ_HANDLER ISR_DMA2_Channel4_5()
+{
+    Dma::serveIsr(&DMA2, 3, Dma::Ch2Handlers);
+    Dma::serveIsr(&DMA2, 4, Dma::Ch2Handlers);
+}
+
---
+++ firmware/master/stm32/dma.h
@@ -0,0 +1,50 @@
+/*
+ * This file is part of the internal implementation of the Sifteo SDK.
+ * Confidential, not for redistribution.
+ *
+ * Copyright <c> 2011 Sifteo, Inc. All rights reserved.
+ */
+
+#ifndef DMA_H_
+#define DMA_H_
+
+#include <stdint.h>
+#include "hardware.h"
+
+class Dma
+{
+public:
+    typedef void (*DmaIsr_t)(void *p, uint32_t flags);
+
+    static void registerHandler(volatile DMA_t *dma, int channel, DmaIsr_t func, void *param);
+    static void unregisterHandler(volatile DMA_t *dma, int channel);
+
+private:
+    struct DmaHandler_t {
+        DmaIsr_t isrfunc;
+        void *param;
+    };
+
+    static void serveIsr(volatile DMA_t *dma, int ch, struct DmaHandler_t *handlers);
+
+    static uint32_t Ch1Mask;
+    static struct DmaHandler_t Ch1Handlers[7];
+
+    static uint32_t Ch2Mask;
+    static struct DmaHandler_t Ch2Handlers[5];
+
+    friend void ISR_DMA1_Channel1();
+    friend void ISR_DMA1_Channel2();
+    friend void ISR_DMA1_Channel3();
+    friend void ISR_DMA1_Channel4();
+    friend void ISR_DMA1_Channel5();
+    friend void ISR_DMA1_Channel6();
+    friend void ISR_DMA1_Channel7();
+
+    friend void ISR_DMA2_Channel1();
+    friend void ISR_DMA2_Channel2();
+    friend void ISR_DMA2_Channel3();
+    friend void ISR_DMA2_Channel4_5();
+};
+
+#endif // DMA_H_
