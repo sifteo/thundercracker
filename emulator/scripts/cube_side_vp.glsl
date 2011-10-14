767650c8cb8a35477f91d5f4ac2ac1f3db69a05d
M. Elizabeth Scott <beth@sifteo.com>
2011-10-13 19:47:43 -0700

    Fix lighting on cube sides
    Was multiplying the normal matrix backwards.. whoops.

--- emulator/scripts/cube_side_vp.glsl
+++ emulator/scripts/cube_side_vp.glsl
@@ -1,6 +1,6 @@
 varying vec3 normal;
 
 void main() {
-     normal = gl_NormalMatrix * gl_Normal;
+     normal = gl_Normal * gl_NormalMatrix;
      gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
 }
