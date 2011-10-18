a051fa3edeeda0e9be6852392b215d13eeb81f4b
M. Elizabeth Scott <beth@sifteo.com>
2011-10-18 13:17:25 -0700

    Cleanup, ahead-of-time asset loading

--- sdk/examples/scroller/main.cpp
+++ sdk/examples/scroller/main.cpp
@@ -4,8 +4,6 @@
  * Copyright <c> 2011 Sifteo, Inc. All rights reserved.
  */
 
-#include <stdio.h>
-#include <string.h>
 #include <sifteo.h>
 #include "assets.gen.h"
 
@@ -13,47 +11,35 @@ using namespace Sifteo;
 
 static Cube cube(0);
 
-static void poke_index(uint16_t addr, uint16_t tile)
+void load()
 {
-    cube.vbuf.poke(addr, ((tile << 1) & 0xFE) | ((tile << 2) & 0xFE00));
+    cube.enable();
+    cube.loadAssets(GameAssets);
+
+    VidMode_BG0_ROM vid(cube.vbuf);
+    vid.init();
+    do {
+        vid.BG0_progressBar(Vec2(0,7), cube.assetProgress(GameAssets, VidMode_BG0::LCD_width) & ~3, 2); 
+        System::paint();
+    } while (!cube.assetDone(GameAssets));
 }
 
 void siftmain()
 {
-    cube.vbuf.init();
+    load();
+    
+    VidMode_BG0 vid(cube.vbuf);
+    vid.init();
+    vid.BG0_drawAsset(Vec2(0,0), Background);
 
-    memset(cube.vbuf.sys.vram.words, 0, sizeof cube.vbuf.sys.vram.words);
-    cube.vbuf.sys.vram.mode = _SYS_VM_BG0_BG1;
-    cube.vbuf.sys.vram.flags = _SYS_VF_CONTINUOUS;
-    cube.vbuf.sys.vram.num_lines = 128;
+    _SYS_vbuf_pokeb(&cube.vbuf.sys, offsetof(_SYSVideoRAM, mode), _SYS_VM_BG0_BG1);
+    _SYS_vbuf_fill(&cube.vbuf.sys, offsetof(_SYSVideoRAM, bg1_bitmap) / 2 + 3, 0x0FF0, 9);
+    _SYS_vbuf_writei(&cube.vbuf.sys, offsetof(_SYSVideoRAM, bg1_tiles) / 2, Overlay.tiles, 0, 8*9);
 
-    for (unsigned y = 0; y < Background.height; y++)
-        for (unsigned x = 0; x < 18; x++)
-            poke_index(offsetof(_SYSVideoRAM, bg0_tiles)/2 +
-                       x + y*18, Background.tiles[x + y*Background.width]);
-
-    for (unsigned y = 0; y < Overlay.height; y++)
-        for (unsigned x = 0; x < Overlay.width; x++)
-            poke_index(offsetof(_SYSVideoRAM, bg1_tiles)/2 +
-                       x + y*Overlay.width,
-                       Overlay.tiles[x + y*Overlay.width]);
-
-    for (unsigned y = 0; y < Overlay.height; y++)
-        cube.vbuf.sys.vram.bg1_bitmap[y+2] = ((1 << Overlay.width) - 1) << 4; 
-
-    cube.enable();
-    cube.loadAssets(GameAssets);
-
-    unsigned t = 0;
-
+    unsigned frame = 0;
     while (1) {
-        t++;
-        if (t == (18*8))
-            t = 0;
-
-        cube.vbuf.pokeb(offsetof(_SYSVideoRAM, bg0_x), t);
-        cube.vbuf.unlock();
-
+        vid.BG0_setPanning(Vec2(frame, 0));
         System::paint();
+        frame++;
     }
 }
