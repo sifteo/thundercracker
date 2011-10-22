d0f3766cf03bb4963c8694e17e182ef8f1e58a66
M. Elizabeth Scott <beth@sifteo.com>
2011-10-21 19:27:41 -0700

    Document tests we don't have yet

--- emulator/scripts/tests.lua
+++ emulator/scripts/tests.lua
@@ -11,14 +11,34 @@ require('scripts/vram')
 
 -- Test code
 require('scripts/test-graphics')
-    
--- You can use environment vars to pass in other options.
--- By default, we run all tests with no GUI.
---
--- In particular, you can set TEST to a string of the form
--- "TestClass:test_name", just like the strings that luainit
--- will print as the tests run.
 
+--[[
+    XXX: There are some big gaps in our testing coverage...
+    
+    * Radio protocol decoder
+    * Flash loadstream decoder
+    * Sensors
+    * Power management?
+    * Recovery from hardware faults, e.g. I2C lockups, bad Flash byte
+    
+    Also, would be nice to have automated code coverage analysis.
+    The emulator's profiling features could be (ab)used to give
+    per-instruction or per-basic-block coverage stats.
+
+    Also also, would be good to have automated performance testing,
+    to catch any performance regressions early, and to quantify
+    any gains we get from optimization.
+]]--
+        
+--[[
+    You can use environment vars to pass in other options.
+    By default, we run all tests with no GUI.
+
+    In particular, you can set TEST to a string of the form
+    "TestClass:test_name", just like the strings that luainit
+    will print as the tests run.
+]]--
+    
 gx:init(os.getenv("USE_FRONTEND"))
 LuaUnit:run(os.getenv("TEST"))
 gx:exit()
