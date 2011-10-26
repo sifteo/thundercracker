3c179b88f0db52e9a47397a3be3bb98ad7148526
Liam Staskawicz <liam@sifteo.com>
2011-10-26 10:26:49 -0700

    * generic AudioOutDevice class, with stm32 specific glue code

---
+++ firmware/master/common/audiooutdevice.h
@@ -0,0 +1,35 @@
+/*
+ * This file is part of the internal implementation of the Sifteo SDK.
+ * Confidential, not for redistribution.
+ *
+ * Copyright <c> 2011 Sifteo, Inc. All rights reserved.
+ */
+
+#ifndef AUDIOOUTDEVICE_H_
+#define AUDIOOUTDEVICE_H_
+
+class AudioMixer;
+
+class AudioOutDevice
+{
+public:
+    enum SampleRate {
+        kHz8000,
+        kHz16000,
+        kHz32000
+    };
+    static void init(SampleRate samplerate, AudioMixer *mixer);
+
+    static void start();
+    static void stop();
+
+    static bool isBusy();
+
+    static int sampleRate();
+    static void setSampleRate(SampleRate samplerate);
+
+    static void suspend();
+    static void resume();
+};
+
+#endif // AUDIOOUTDEVICE_H_
---
+++ firmware/master/stm32/audiooutdevice.cpp
@@ -0,0 +1,70 @@
+
+#include "audiooutdevice.h"
+#include "pwmaudioout.h"
+
+#include "hardware.h"
+#include "gpio.h"
+#include "hwtimer.h"
+
+static const int pwmChan = 2;
+static PwmAudioOut pwmAudioOut(HwTimer(&TIM1), pwmChan, HwTimer(&TIM4),
+                                GPIOPin(&GPIOA, 9), GPIOPin(&GPIOB, 0));
+
+/*
+ * STM32 implementation of AudioOutDevice - just forward calls to the
+ * appropriate hw specific device. This sucks at the moment, since
+ * we're paying for function call overhead, according to the map file - even at O2.
+ *
+ * There's probably a better way to organize this, but this at least
+ * allows us to keep the same audiooutdevice.h between the cube and the
+ * simulator.
+ */
+
+#if 1
+IRQ_HANDLER ISR_TIM4()
+{
+    TIM4.SR = 0; // clear status to acknowledge the ISR
+    pwmAudioOut.tmrIsr();
+}
+#endif
+
+void AudioOutDevice::init(SampleRate samplerate, AudioMixer *mixer)
+{
+    pwmAudioOut.init(samplerate, mixer);
+}
+
+void AudioOutDevice::start()
+{
+    pwmAudioOut.start();
+}
+
+void AudioOutDevice::stop()
+{
+    pwmAudioOut.stop();
+}
+
+bool AudioOutDevice::isBusy()
+{
+    return pwmAudioOut.isBusy();
+}
+
+int AudioOutDevice::sampleRate()
+{
+    return pwmAudioOut.sampleRate();
+}
+
+void AudioOutDevice::setSampleRate(SampleRate samplerate)
+{
+//    pwmAudioOut.setSampleRate(samplerate); // not yet implemented
+    (void)samplerate;
+}
+
+void AudioOutDevice::suspend()
+{
+    pwmAudioOut.suspend();
+}
+
+void AudioOutDevice::resume()
+{
+    pwmAudioOut.resume();
+}
