bff2901721493216b6f1d21d48a0b2ecb62c6ea9
M. Elizabeth Scott <beth@sifteo.com>
2011-10-28 05:31:39 -0700

    Fix paintSync() for reals.

--- firmware/master/common/cube.cpp
+++ firmware/master/common/cube.cpp
@@ -388,16 +388,29 @@ void CubeSlot::waitForFinish()
      */
 
     uint8_t flags = VRAM::peekb(*vbuf, offsetof(_SYSVideoRAM,flags));
-    flags &= ~_SYS_VF_CONTINUOUS;
-    VRAM::pokeb(*vbuf, offsetof(_SYSVideoRAM, flags), flags);
-    VRAM::unlock(*vbuf);
-        
+    if (flags & _SYS_VF_CONTINUOUS) {
+        /*
+         * If we were in continuous rendering mode, pendingFrames isn't
+         * exact; it's more of a running accumulator. In that case, we have
+         * to turn off continuous rendering mode, flush everything over the
+         * radio, then do one Paint(). This is what paintSync() does.
+         * So, at this point we just need to reset pendingFrames to zero
+         * and turn off continuous mode.
+         */
+         
+        pendingFrames = 0;
+
+        VRAM::pokeb(*vbuf, offsetof(_SYSVideoRAM, flags), flags & ~_SYS_VF_CONTINUOUS);
+        VRAM::unlock(*vbuf);
+    }
+     
     for (;;) {
         Atomic::Barrier();
         SysTime::Ticks now = SysTime::ticks();
 
         if (now > (paintTimestamp + fpsLow)) {
-            // Watchdog expired. Give up waiting.
+            // Watchdog expired. Give up waiting, and forcibly reset pendingFrames.
+            pendingFrames = 0;
             break;
         }
 
