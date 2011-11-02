652db3cce102d1a9fc148cc40d02e5b39415b5d4
Max Kaufmann <max.kaufmann@gmail.com>
2011-11-01 20:32:27 -0700

    started porting GameView code over to Cube

--- sdk/include/sifteo/cube.h
+++ sdk/include/sifteo/cube.h
@@ -10,7 +10,14 @@
 #include <sifteo/asset.h>
 #include <sifteo/video.h>
 
-#define CUBE_ID_UNDEFINED ((_SYSCubeID)-1)
+#define CUBE_ID_UNDEFINED (0xff)
+#define NUM_SIDES (4)
+#define SIDE_TOP (0)
+#define SIDE_LEFT (1)
+#define SIDE_BOTTOM (2)
+#define SIDE_RIGHT (3)
+#define SIDE_UNDEFINED (0xff)
+
 
 namespace Sifteo {
 
@@ -26,6 +33,7 @@ namespace Sifteo {
 class Cube {
  public:
     typedef _SYSCubeID ID;
+    typedef uint8_t Side;
 	
 	Cube()
 		: mID(CUBE_ID_UNDEFINED) {}
@@ -42,6 +50,10 @@ class Cube {
 		if (id != CUBE_ID_UNDEFINED) {
 			mID = id;
 		}
+        ASSERT(mID != CUBE_ID_UNDEFINED);
+        for(int i=0; i<NUM_SIDES; ++i) {
+            mNeighbors[i] = 0;
+        }
         vbuf.init();
         _SYS_setVideoBuffer(mID, &vbuf.sys);
         _SYS_enableCubes(Intrinsic::LZ(mID));
@@ -72,11 +84,68 @@ class Cube {
     ID id() const {
         return mID;
     }
+    
+    Cube* physicalNeighborAt(Side side) const {
+        return mNeighbors[side];
+    }
+    
+    Side physicalSideOf(Cube* cube) const {
+        for(Side side=0; side<NUM_SIDES; ++side) {
+            if (mNeighbors[side] == cube) {
+                return side;
+            }
+        }
+        return SIDE_UNDEFINED;
+    }
 
+    VidMode::Rotation rotation() const {
+        const uint8_t mask = _SYS_VF_XY_SWAP | _SYS_VF_X_FLIP | _SYS_VF_Y_FLIP;
+        uint8_t flags = vbuf.peekb(offsetof(_SYSVideoRAM, flags));
+        return (VidMode::Rotation)(flags & mask);
+    }
+    
+    Side orientation() const {
+        switch(rotation()) {
+            case VidMode::ROT_NORMAL: return SIDE_TOP;
+            case VidMode::ROT_LEFT_90: return SIDE_LEFT;
+            case VidMode::ROT_180: return SIDE_BOTTOM;
+            case VidMode::ROT_RIGHT_90: return SIDE_RIGHT;
+            default: return SIDE_UNDEFINED;
+        }
+    }
+    
+    Cube* virtualNeighborAt(Side side) const {
+        Side rot = orientation();
+        if (rot == SIDE_UNDEFINED) { return 0; }
+        side = (side + rot) % NUM_SIDES;
+        return physicalNeighborAt(side);
+    }
+    
+    Side virtualSideOf(Cube* cube) const {
+        int side = physicalSideOf(cube);
+        if (side == SIDE_UNDEFINED) { return SIDE_UNDEFINED; }
+        Side rot = orientation();
+        if (rot == SIDE_UNDEFINED) { return SIDE_UNDEFINED; }
+        side = (side - rot) % NUM_SIDES;
+        return side < 0 ? side + NUM_SIDES : side;
+    }
+    
     VideoBuffer vbuf;
 
  private:
     ID mID;
+    Cube* mNeighbors[NUM_SIDES];
+    
+    void didAddNeighbor(Cube* cube, Side mySide, Side theirSide) {
+        ASSERT(mNeighbors[mySide] == 0);
+        mNeighbors[mySide] = cube;
+    }
+    
+    void willRemoveNeighbor(Cube* cube, Side mySide, Side theirSide) {
+        ASSERT(mNeighbors[mySide] == cube);
+        mNeighbors[mySide] = 0;
+    }
+    
 };
 
 
