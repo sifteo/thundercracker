ce34358fceff3c2612baf06a96c2da17bbf37124
Liam Staskawicz <liam@sifteo.com>
2011-11-09 13:46:48 -0800

    * use speex 1.2rc1 for FW speex lib, C sources only - asm optimized versions from ST don't seem to work.

--- firmware/master/speex/speex.mk
+++ firmware/master/speex/speex.mk
@@ -7,6 +7,36 @@ SPEEX_OBJS_STM32 = \
     $(SPEEX_DIR)/libspeex/window.stm32.o \
     $(SPEEX_DIR)/libspeex/bits.stm32.o \
     $(SPEEX_DIR)/libspeex/exc_10_32_table.stm32.o \
+    $(SPEEX_DIR)/libspeex/exc_10_16_table.stm32.o \
+    $(SPEEX_DIR)/libspeex/exc_20_32_table.stm32.o \
+    $(SPEEX_DIR)/libspeex/exc_5_64_table.stm32.o \
+    $(SPEEX_DIR)/libspeex/exc_8_128_table.stm32.o \
+    $(SPEEX_DIR)/libspeex/exc_5_256_table.stm32.o \
+    $(SPEEX_DIR)/libspeex/gain_table_lbr.stm32.o \
+    $(SPEEX_DIR)/libspeex/gain_table.stm32.o \
+    $(SPEEX_DIR)/libspeex/lpc.stm32.o \
+    $(SPEEX_DIR)/libspeex/lsp.stm32.o \
+    $(SPEEX_DIR)/libspeex/speex.stm32.o \
+    $(SPEEX_DIR)/libspeex/speex_callbacks.stm32.o \
+    $(SPEEX_DIR)/libspeex/speex_header.stm32.o \
+    $(SPEEX_DIR)/libspeex/modes.stm32.o \
+    $(SPEEX_DIR)/libspeex/nb_celp.stm32.o \
+    $(SPEEX_DIR)/libspeex/quant_lsp.stm32.o \
+    $(SPEEX_DIR)/libspeex/vq.stm32.o \
+    $(SPEEX_DIR)/libspeex/cb_search.stm32.o \
+    $(SPEEX_DIR)/libspeex/filters.stm32.o \
+    $(SPEEX_DIR)/libspeex/ltp.stm32.o \
+    $(SPEEX_DIR)/libspeex/buffer.stm32.o \
+    $(SPEEX_DIR)/libspeex/high_lsp_tables.stm32.o \
+    $(SPEEX_DIR)/libspeex/lsp_tables_nb.stm32.o
+
+# NOTE - I have had trouble with the cortex-m3 optimized version that ships with ST app note 2812.
+# Decoding results in all zeros :( For now, use the C version above from the standard 
+# speex download, and come back to resolve this. 
+SPEEX_OBJS_CORTEX_M3 = \
+    $(SPEEX_DIR)/libspeex/window.stm32.o \
+    $(SPEEX_DIR)/libspeex/bits.stm32.o \
+    $(SPEEX_DIR)/libspeex/exc_10_32_table.stm32.o \
     $(SPEEX_DIR)/libspeex/gain_table_lbr.stm32.o \
     $(SPEEX_DIR)/libspeex/lpc.stm32.o \
     $(SPEEX_DIR)/libspeex/lsp.stm32.o \
@@ -31,6 +61,7 @@ SPEEX_INC = \
     -I$(SPEEX_DIR) \
     -I$(SPEEX_DIR)/include \
     -I$(SPEEX_DIR)/STM32 \
+    -I$(SPEEX_DIR)/STM32/include \
     -I$(SPEEX_DIR)/STM32/libspeex/gcc \
     -I$(SPEEX_DIR)/libspeex
 
