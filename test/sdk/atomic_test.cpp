6d2e8cec02c1d23477f7d2b09504a31179ac497f
Jared Hanson <jaredhanson@gmail.com>
2011-11-17 10:02:08 -0800

    More tests for atomics in SDK.

--- test/sdk/atomic_test.cpp
+++ test/sdk/atomic_test.cpp
@@ -28,6 +28,18 @@ TEST(AtomicTest, UInt32StoreWorks) {
     EXPECT_EQ(UINT32_MAX, dest);
 }
 
+TEST(AtomicTest, AndWorks) {
+    uint32_t dest = 0x000000FF;
+    Atomic::And(dest, 0xFF0000F0);
+    EXPECT_EQ(0x000000F0, dest);
+}
+
+TEST(AtomicTest, OrWorks) {
+    uint32_t dest = 0x000000FF;
+    Atomic::Or(dest, 0xFF0000F0);
+    EXPECT_EQ(0xFF0000FF, dest);
+}
+
 TEST(AtomicTest, SetBitWorks) {
     uint32_t dest = 0x000000FF;
     Atomic::SetBit(dest, 18);
@@ -40,6 +52,19 @@ TEST(AtomicTest, ClearBitWorks) {
     EXPECT_EQ(0x000000FF, dest);
 }
 
+TEST(AtomicTest, Int32AddWorks) {
+    int32_t dest = 1032;
+    Atomic::Add(dest, 314);
+    EXPECT_EQ(1346, dest);
+}
+
+TEST(AtomicTest, UInt32AddWorks) {
+    uint32_t dest = INT32_MAX;
+    Atomic::Add(dest, 314);
+    EXPECT_EQ(2147483961, dest);
+}
+
+// FIXME: Not sure the purpose of this function, so not writting tests at the moment.
 /*
 TEST(AtomicTest, SetLZWorks) {
     uint32_t dest = 0x220000FF;
@@ -51,6 +76,7 @@ TEST(AtomicTest, SetLZWorks) {
 }
 */
 
+// FIXME: Not sure the purpose of this function, so not writting tests at the moment.
 /*
 TEST(AtomicTest, ClearLZWorks) {
     uint32_t dest = 0x220000FF;
