9eab336f2dcafdc3f50629102121a1f70dbfb9d2
Chris March <chris@sifteo.com>
2011-12-06 15:05:47 -0800

    HQ images for merge to master

--- sdk/examples/word/assets.lua
+++ sdk/examples/word/assets.lua
@@ -1,14 +1,14 @@
 GameAssets = group{}
 
-BGNotWordNotConnected = image{"ering.png", quality=0}
-BGNotWordConnected = image{"ering_not_a_word.png", quality=0}
-BGNewWordConnectedMiddle = image{"ering_new_word_mid.png", quality=0}
-BGNewWordConnectedLeft = image{"ering_new_word.png", quality=0}
-BGNewWordConnectedRight = image{"ering_new_word_r.png", quality=0}
-BGOldWordConnectedMiddle = image{"ring_connected_mid.png", quality=0}
-BGOldWordConnectedLeft = image{"ring_connected.png", quality=0}
-BGOldWordConnectedRight = image{"ring_connected_r.png", quality=0}
-StartScreen = image{"start_screen.png", quality=0}
-Title = image{"title.png", quality=0}
-Font = image{"font-32x64.png", width=32, height=64, quality=1}
-FontSmall = image{"font-8x16.png", width=8, height=16, quality=1}
+BGNotWordNotConnected = image{"ering.png", quality=10}
+BGNotWordConnected = image{"ering_not_a_word.png", quality=10}
+BGNewWordConnectedMiddle = image{"ering_new_word_mid.png", quality=10}
+BGNewWordConnectedLeft = image{"ering_new_word.png", quality=10}
+BGNewWordConnectedRight = image{"ering_new_word_r.png", quality=10}
+BGOldWordConnectedMiddle = image{"ring_connected_mid.png", quality=10}
+BGOldWordConnectedLeft = image{"ring_connected.png", quality=10}
+BGOldWordConnectedRight = image{"ring_connected_r.png", quality=10}
+StartScreen = image{"start_screen.png", quality=10}
+Title = image{"title.png", quality=10}
+Font = image{"font-32x64.png", width=32, height=64, quality=10}
+FontSmall = image{"font-8x16.png", width=8, height=16, quality=10}
