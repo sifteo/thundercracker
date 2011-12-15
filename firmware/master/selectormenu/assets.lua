1b0f7110cf405ea4371900d79cf28047245993d1
Eric Liao <eric@sifteo.com>
2011-12-15 15:51:34 -0800

    move tiltflow to firmware.  It's optionally built in.

---
+++ firmware/master/selectormenu/assets.lua
@@ -0,0 +1,12 @@
+MenuControllerAssets = group{quality=10}
+IconFire = image{"selectormenu/icon_elem_fire.png"}
+IconEarth = image{"selectormenu/icon_elem_earth.png"}
+IconWater = image{"selectormenu/icon_elem_water.png"}
+TextFire = image{"selectormenu/TextFire.png"}
+TextEarth = image{"selectormenu/TextEarth.png"}
+TextWater = image{"selectormenu/TextWater.png"}
+Tip1 = image{"selectormenu/Tip1.png"}
+Tip2 = image{"selectormenu/Tip2.png"}
+Tip3 = image{"selectormenu/Tip3.png"}
+--for clearing
+White = image{"selectormenu/White.png"}
