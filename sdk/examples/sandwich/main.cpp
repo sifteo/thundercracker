24350c0634d8b47f5b5a9be6d8f3a57c6dcbcdb6
Max Kaufmann <max.kaufmann@gmail.com>
2011-12-16 15:22:28 -0800

    more cleanup

--- sdk/examples/sandwich/main.cpp
+++ sdk/examples/sandwich/main.cpp
@@ -4,6 +4,7 @@ static Game sGame;
 Game* pGame = &sGame;
 
 void siftmain() {
+	// TODO: Move Cubes out of GameView to separate Alloc?
 	{ // initialize assets
 	  for (Cube::ID i = 0; i < NUM_CUBES; i++) {
 	    sGame.CubeAt(i)->enable(i);
@@ -12,18 +13,15 @@ void siftmain() {
 	    rom.init();
 	    rom.BG0_text(Vec2(1,1), "Loading...");
 	  }
-	  for (;;) {
-	    bool done = true;
+	  bool done = false;
+	  while(!done) {
+	  	done = true;
 	    for (Cube::ID i = 0; i < NUM_CUBES; i++) {
 	      VidMode_BG0_ROM rom(sGame.CubeAt(i)->vbuf);
 	      rom.BG0_progressBar(Vec2(0,7), sGame.CubeAt(i)->assetProgress(GameAssets, VidMode_BG0::LCD_width), 2);
-	      if (!sGame.CubeAt(i)->assetDone(GameAssets))
-	        done = false;
+	      done &= sGame.CubeAt(i)->assetDone(GameAssets);
 	    }
 	    System::paint();
-	    if (done) {
-	      break;
-			}
 	  }
 	  for(Cube::ID i=0; i<NUM_CUBES; ++i) {
 	    VidMode_BG0 mode(sGame.CubeAt(i)->vbuf);
