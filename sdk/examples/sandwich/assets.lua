139136ab87ec1a90ecb12fc4c69b3dea354c4607
Max Kaufmann <max.kaufmann@gmail.com>
2011-12-19 10:46:08 -0800

    exported win-screen

--- sdk/examples/sandwich/assets.lua
+++ sdk/examples/sandwich/assets.lua
@@ -7,4 +7,8 @@ Sting = image{"sting.png",width=128,height=128}
 Skeleton = image{"skeleton.png", width=32, height=32,pinned=true}
 Items = image{"items.png", width=16, height=16, pinned=true}
 
+Winscreen = image{"winscreen.png", width=128, height=128}
+WinscreenBackground = image{"winscreen_background.png", width=128, height=128}
+WinscreenAnim = image{"winscreen_anim.png", width=56, height=80, quality=3}
+
 dofile "gen_assets.lua"
