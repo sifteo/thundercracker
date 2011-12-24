284f8439127d99797342febb9c2b6c4f9bb9da45
Liam Staskawicz <liam@sifteo.com>
2011-12-23 16:08:37 -0800

    * move some more USB handling into usb.cpp * simplify usb_conf.h - don't need any non-stm32f105 code * don't use SOF interrupt at the moment

--- firmware/master/stm32/usb.cpp
+++ firmware/master/stm32/usb.cpp
@@ -2,9 +2,27 @@
 #include "hardware.h"
 #include "usb_init.h"
 #include "usb_istr.h"
+#include "otgd_fs_cal.h"
+#include "otgd_fs_pcd.h"
+#include "usb_sil.h"
 
 void Usb::init() {
     USB_Init();
+}
+
+// defined in usb_istr.h
+void EP1_IN_Callback(void)
+{
+
+}
+
+// defined in usb_istr.h
+void EP3_OUT_Callback(void)
+{
+    uint8_t buffer[64];
+    uint16_t count = USB_SIL_Read(EP3_OUT, buffer);
+
+    USB_SIL_Write(EP1_IN, buffer, count);
 }
 
 IRQ_HANDLER ISR_UsbOtg_FS() {
--- firmware/master/stm32/usb.h
+++ firmware/master/stm32/usb.h
@@ -1,6 +1,15 @@
 #ifndef USB_H
 #define USB_H
 
+#define EP0_OUT                    0x00
+#define EP0_IN                     0x80
+#define EP1_OUT                    0x01
+#define EP1_IN                     0x81
+#define EP2_OUT                    0x02
+#define EP2_IN                     0x82
+#define EP3_OUT                    0x03
+#define EP3_IN                     0x83
+
 class Usb
 {
 public:
