e5e8d5907876f611ce4cbb6bb32529336f992dc5
Josh Lee <josh@sifteo.com>
2012-01-02 11:57:06 -0800

    add logo screen

--- firmware/master/selectormenu/assets.lua
+++ firmware/master/selectormenu/assets.lua
@@ -2,12 +2,10 @@ MenuControllerAssets = group{quality=10}
 IconGameChroma = image{"selectormenu/icon_game_cs.png"}
 IconGameWord = image{"selectormenu/icon_game_wp.png"}
 IconGameSandwich = image{"selectormenu/icon_game_sk.png"}
---IconGamePeano = image{"selectormenu/icon_game_pv.png"}
 IconGameMore = image{"selectormenu/icon_game_more.png"}
 LabelChroma = image{"selectormenu/label_cs.png"}
 LabelWord = image{"selectormenu/label_wp.png"}
 LabelSandwich = image{"selectormenu/label_sk.png"}
---LabelPeano = image{"selectormenu/label_pv.png"}
 LabelMore = image{"selectormenu/label_more.png"}
 Tip1 = image{"selectormenu/Tip1.png"}
 Tip2 = image{"selectormenu/Tip2.png"}
@@ -16,3 +14,4 @@ Tip3 = image{"selectormenu/Tip3.png"}
 White = image{"selectormenu/White.png"}
 --game covers
 ChromaCover = image{"selectormenu/cover.png"}
+LogoCover = image{"selectormenu/logo.png"}
