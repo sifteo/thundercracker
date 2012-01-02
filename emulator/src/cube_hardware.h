ac5153d3efce2663eb13e849a470d6bcf0664375
M. Elizabeth Scott <beth@sifteo.com>
2012-01-02 13:15:57 -0800

    Swap LAT1 and LAT2, to match current PCB revs
    All tests pass!

--- emulator/src/cube_hardware.h
+++ emulator/src/cube_hardware.h
@@ -69,8 +69,8 @@ static const uint8_t BUS_PORT_DIR    = REG_P2DIR;
 static const uint8_t CTRL_PORT_DIR   = REG_P3DIR;
 
 static const uint8_t CTRL_LCD_DCX    = (1 << 0);
-static const uint8_t CTRL_FLASH_LAT1 = (1 << 1);
-static const uint8_t CTRL_FLASH_LAT2 = (1 << 2);
+static const uint8_t CTRL_FLASH_LAT1 = (1 << 2);   // LAT1 and LAT2 swapped on current rev PCBs vs. prototype
+static const uint8_t CTRL_FLASH_LAT2 = (1 << 1);
 static const uint8_t CTRL_FLASH_WE   = (1 << 5);
 static const uint8_t CTRL_FLASH_OE   = (1 << 6);
 
