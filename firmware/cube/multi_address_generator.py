88b6ab65037e06a7de13f33b079ca6b56cfd9e8b
Jared Wolff <jwolff@sifteo.com>
2012-01-02 15:42:55 -0800

    Adding binary generation script to master. Generates cube binary files with addresses from 0x00 to 0x05.

---
+++ firmware/cube/multi_address_generator.py
@@ -0,0 +1,36 @@
+#
+# Python script to execute the compilation
+# of TC cube firmware utilizing different hard coded
+# addresses.
+#
+# Sifteo 2012 
+#
+# Jared Wolff - Sifteo Hardware Engineering
+
+import sys
+import os
+import shutil
+import datetime
+import platform
+import subprocess
+
+date = datetime.date.today().isoformat()
+
+if platform.system() == 'Windows':
+	
+	print "Windows"
+	
+elif platform.system() == 'Darwin' or platform.system == 'Linux':
+	
+	print "\nCube Binary Generator\nCompiling firmare 0x00 to 0x05...\n"
+	
+	for addr in ["0x00","0x01","0x02","0x03","0x04","0x05"]:
+		subprocess.check_call(["make","clean"])
+		myenv = dict(os.environ)
+		myenv["CFLAGS"] = "-DCUBE_ADDR="+addr
+		subprocess.check_call(["make"], env=myenv)
+		shutil.move("cube.ihx", "cube_"+date+"_addr_"+addr+".hex")
+	
+else:
+	print "Unsupported system!"
+	sys.exit(0)
