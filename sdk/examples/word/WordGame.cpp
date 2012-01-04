76251a675d86fa26b4db5ba374a9d98f44f956cc
Chris March <chris@sifteo.com>
2012-01-03 17:19:53 -0800

    re-enabled music, and fixed some asset sampling issue

--- sdk/examples/word/WordGame.cpp
+++ sdk/examples/word/WordGame.cpp
@@ -47,10 +47,12 @@ void WordGame::_onEvent(unsigned eventID, const EventData& data)
 
 bool WordGame::playAudio(const _SYSAudioModule &mod, AudioChannelIndex channel , _SYSAudioLoopType loopMode)
 {
+    /* FIXME remove
     if (channel == AudioChannelIndex_Music)
     {
         return false;
     }
+    */
     ASSERT(sInstance);
     return sInstance->_playAudio(mod, channel, loopMode);
 }
