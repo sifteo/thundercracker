4731fc9e57dc03341c66099d1049fee4cafee446
M. Elizabeth Scott <beth@sifteo.com>
2012-01-05 17:25:20 -0800

    Invert accelerometer polarity in master firmware
    This lets us keep the radio protocol identical to hardware
    for now at least, and it gives the SDK a polarity that is
    intuitive and which matches old behaviour.

--- firmware/master/common/cube.cpp
+++ firmware/master/common/cube.cpp
@@ -293,8 +293,9 @@ void CubeSlot::radioAcknowledge(const PacketBuffer &packet)
     if (packet.len >= offsetof(RF_ACKType, accel) + sizeof ack->accel) {
         // Has valid accelerometer data. Is it different from our previous state?
 
-        int8_t x = ack->accel[0];
-        int8_t y = ack->accel[1];
+        // Translate from radio packet coordinates to SDK coordinates
+        int8_t x = -ack->accel[0];
+        int8_t y = -ack->accel[1];
 
 		//test for gestures
 		AccelState &accel = AccelState::getInstance( id() );
