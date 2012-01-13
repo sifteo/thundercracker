f0695f05e11dd8d9feff609009704aa0d667ce7a
M. Elizabeth Scott <beth@sifteo.com>
2012-01-13 15:53:51 -0800

    Fix lightmap banding
    The 8-bit light map had visible banding, which looked especially
    bad in full-screen. This was purely due to the limited resolution
    of the image file.
    
    Part of the problem was that the original lightmap only used a
    small portion of the dynamic range available to it. This change
    modifies the lightmap to use the full 8-bit dynamic range, and it
    adds ambient lighting as a separate term in the fragment shader.

--- emulator/resources/background_fp.glsl
+++ emulator/resources/background_fp.glsl
@@ -4,6 +4,9 @@ varying vec2 eyeCoord;
 uniform sampler2D texture;
 uniform sampler2D lightmap;
 
+const float AMBIENT = 0.4;
+const float SPECULAR = 0.7;
+
 void main() {
-    gl_FragColor = texture2D(texture, texCoord) * texture2D(lightmap, eyeCoord).r;
+    gl_FragColor = texture2D(texture, texCoord) * (AMBIENT + SPECULAR * texture2D(lightmap, eyeCoord).r);
 }
