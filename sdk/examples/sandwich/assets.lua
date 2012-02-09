13322680952339c4dcf2922d257887e0928b3fc3
Max Kaufmann <max.kaufmann@gmail.com>
2012-02-08 16:11:45 -0800

    saving work

--- sdk/examples/sandwich/assets.lua
+++ sdk/examples/sandwich/assets.lua
@@ -1,34 +1,38 @@
-GameAssets = group{quality=10}
+GameAssets = group{quality=9}
 
-ScreenOff = image{"PowerOff.png", width=128, height=128}
-
-Blank = image{"Blank.png", width=128, height=128}
+Blank = image{"Blank.png", width=128, height=128, quality=7}
+Sting = image{"sting.png",width=128,height=128}
+Title = image{"title.png"}
+TitleBalloon = image{"title_bubble.png"}
+TitleThoughts = image{"title_thought_dots.png"}
 
 PlayerStand = image{"stand.png", width=32, height=32,pinned=true}
 PlayerWalk = image{"walk.png", width=32, height=32,pinned=true}
 PlayerPickup = image{"pickup.png", width=32, height=32, pinned=true}
 PlayerIdle = image{"idle.png", width=32, height=32, pinned=true}
-Sting = image{"sting.png",width=128,height=128}
 Butterfly = image{"butterfly.png", width=8, height=8, pinned=true}
 Items = image{"items.png", width=16, height=16, pinned=true}
 
+--[[
 Winscreen = image{"winscreen.png", width=128, height=128}
 WinscreenBackground = image{"winscreen_background.png", width=128, height=128}
 WinscreenAnim = image{"winscreen_anim.png", width=56, height=80, quality=6}
+]]--
 
 Sparkle = image{"sparkle.png", width=8, height=8, pinned=true}
 
 Flash = image{"flash.png", width=8, height=8}
 Black = image{"black.png", width=8, height=8}
 
+--[[
 ScrollLeft = image{"scroll_left.png", width=16, height=128}
 ScrollLeftAccent = image{"scroll_left_accent.png", width=8, height=128}
 ScrollMiddle = image{"scroll_middle.png", width=8, height=128}
 ScrollRightAccent = image{"scroll_right_accent.png", width=8, height=128}
 ScrollRight = image{"scroll_right.png", width=16, height=128}
-
 ScrollBubble = image{"scroll_bubble.png", width=80, height=32}
 ScrollThoughts = image{"scroll_thoughts.png", width=16, height=16}
+]]--
 
 InventoryBackground = image{"inventory_background.png"}
 
