54eaf52b2b7351626d8154ec3b850b685522ca7d
Max Kaufmann <max.kaufmann@gmail.com>
2012-02-16 18:07:23 -0800

    added some assertions

--- sdk/examples/sandwich/sandwich_item.py
+++ sdk/examples/sandwich/sandwich_item.py
@@ -5,12 +5,18 @@ class ItemDatabase:
 		self.world = world
 		self.path = path
 		doc = lxml.etree.parse(path)
-		self.items = [ Item(i,xml) for i,xml in enumerate(doc.findall("item")) ]
-		self.item_dict = dict((item.id, item) for item in self.items)
+		items = [ Item(xml) for xml in doc.findall("item") ]
+		self.items = [ None for _ in range(len(items)) ]
+		for item in items:
+			assert item.icon < len(items), "Icon out of range"
+			self.items[item.icon] = item
+		
+		self.item_dict = dict((item.id, item) for item in items)
+
+
 
 class Item:
-	def __init__(self, index, xml):
-		self.index = index
+	def __init__(self, xml):
 		self.icon = int(xml.get("icon", ""))
 		self.id = xml.get("id").lower()
 		self.name = xml.find("name").text.strip()
