6ed4be4ee5930f9c7de931c86829b66eb61cd513
unknown <Kevin@Peach.(none)>
2012-02-23 18:59:03 -0800

    almost done with database processing

--- sdk/examples/peano/mungePuzzles.py
+++ sdk/examples/peano/mungePuzzles.py
@@ -11,8 +11,65 @@ def side(text):
 def guidToInts(s):
 	return struct.unpack("IIII", uuid.UUID(s).bytes)
 
+def writeGuid(s, f):
+	guid = uuid.UUID(s)
+	bytes = guid.bytes
+	int32s = struct.unpack("IIII", bytes)
+	hexes = map(hex, int32s)
+	hexes = map(lambda a:a.replace("L",""), hexes)
+	f.write( "{\n\t" )
+	f.write( hexes[0] + ", " + hexes[1] + ", " + hexes[2] + ", " + hexes[3] )
+	f.write( "}\n" )
+	
+
+#write out the header file
+header = open("puzzles.gen.h", "wt")
+header.write("#pragma once\n")
+header.write("#include \"assets.gen.h\"\n" )
+
+header.write("namespace TotalsGame\n{\nnamespace TheData\n{\n" )
+
+header.write("struct PuzzleDef\n")
+header.write("{\n")
+header.write("\tconst uint8_t *tokens;\n")
+header.write("\tconst uint8_t nTokens;\n")
+header.write("\tconst uint16_t *groups;\n")
+header.write("\tconst uint8_t nGroups;\n")
+header.write("\tconst uint32_t guid[4];\n")
+header.write("};\n")
+
+header.write("struct ChapterDef\n")
+header.write("{\n")
+header.write("\tconst PuzzleDef *puzzles;\n")
+header.write("\tconst uint8_t nPuzzles;\n")
+header.write("\tconst AssetImage &icon;\n")
+header.write("\tconst char *name;\n")
+header.write("\tconst uint32_t guid[4];\n")
+header.write("};\n")
+
+header.write("struct EverythingDef\n")
+header.write("{\n")
+header.write("\tconst ChapterDef *chapters;\n")
+header.write("\tconst uint8_t nChapters;\n")
+header.write("};\n")
+
+header.write("extern EverythingDef everything\n")
+
+header.write("}\n}\n")
+
+#do all the fancy stuff here
+
+f = open("puzzles.gen.c", "wt")
+
+f.write("#include \"puzzles.gen.h\"\n")
+
+f.write("namespace TotalsGame\n{\nnamespace TheData\n{\n" )
+
 document = xml.dom.minidom.parse("puzzles.xml")
 chapters = document.getElementsByTagName("chapters")[0].getElementsByTagName("chapter")
+
+chapterIndex = 0
+
 for chapter in chapters:
 
 	id = chapter.getAttribute("id")
@@ -20,6 +77,8 @@ for chapter in chapters:
 	guid = chapter.getAttribute("guid")
 
 	print id, name, guid
+
+	puzzleIndex = 0
 
 	puzzles = chapter.getElementsByTagName("puzzle")
 	for puzzle in puzzles:
@@ -31,16 +90,16 @@ for chapter in chapters:
 		tokens = puzzle.getElementsByTagName("token")
 		groups = puzzle.getElementsByTagName("group")
 
-		print "uint8_t tokens[]={",
+		f.write( "const uint8_t "+"c"+str(chapterIndex)+"p"+str(puzzleIndex)+"tokens[] =\n{\n\t" )
 		for token in tokens:
 			val = int(token.getAttribute("val"))
 			opRight = op(token.getAttribute("opRight"))
 			opBottom = op(token.getAttribute("opBottom"))
 			packedByte = val | opRight<<4 | opBottom<<6
-			print hex(packedByte), ", ", 
-		print "}"
+			f.write( hex(packedByte) + ", ", )
+		f.write( "\n};\n" )
 
-		print "uint16_t groups[]={",
+		f.write( "const uint16_t "+"c"+str(chapterIndex)+"p"+str(puzzleIndex)+"groups[] =\n{\n\t" )
 		for group in groups:
 			src = int(group.getAttribute("src"))
 			srcToken = int(group.getAttribute("srcToken"))
@@ -48,5 +107,43 @@ for chapter in chapters:
 			dst = int(group.getAttribute("dst"))
 			dstToken = int(group.getAttribute("dstToken"))
 			packedShort = src*10+srcToken | srcSide<<7 | dst*10+dstToken<<9
-			print hex(packedShort), ", ",
-		print "}"
+			f.write( hex(packedShort) + ", ",  )
+		f.write( "\n};\n" )
+
+
+		f.write( "const PuzzleDef c" + str(chapterIndex) + "puzzle" + str(puzzleIndex) +" ="+ "\n{\n" )
+		f.write( "\t c"+str(chapterIndex)+"p"+str(puzzleIndex)+"tokens"+ ", " )
+		f.write( str(len(tokens)) + ", " )
+		f.write( "c"+str(chapterIndex)+"p"+str(puzzleIndex)+"groups"+ ", " )
+		f.write( str(len(groups)) + "\n};\n" )
+
+		puzzleIndex = puzzleIndex + 1
+
+	f.write( "const PuzzleDef *c" + str(chapterIndex) + "puzzles[]" +" ="+ "\n{\n\t" )
+	for i in range(len(puzzles)):
+		f.write("&c"+str(chapterIndex) + "puzzle" + str(i) +", ")
+	f.write( "\n};\n" )
+
+
+	f.write( "const ChapterDef chapter" + str(chapterIndex) + " =" + "\n{\n" )
+	f.write( "\t&c"+str(chapterIndex)+"puzzles" + ", ")
+	f.write( str(len(puzzles)) + ", ")
+	f.write( ", \"icon_" + id + "\", \"" + name + "\"" + "\n};\n" )
+
+
+	chapterIndex = chapterIndex + 1
+
+f.write( "const ChapterDef *chapters[] =\n{\n\t" )
+for i in range(len(chapters)):
+	f.write("&chapter"+str(i) +", ")
+f.write( "\n};\n" )
+
+f.write( "const EverythingDef everything" + " =" + "\n{\n" )
+f.write( "\t&chapters"+", " )
+f.write( str(len(chapters)) + "\n};\n" )
+
+
+f.write("}\n}\n" )
+
+f.close()
+header.close()
