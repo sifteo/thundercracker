29a90d0ef3c7bdf6dff5b7d58fedd30500f36470
Max Kaufmann <max.kaufmann@gmail.com>
2012-03-04 23:02:48 -0800

    saving work

--- sdk/examples/betterflow/main.cpp
+++ sdk/examples/betterflow/main.cpp
@@ -2,37 +2,28 @@
 #include "assets.gen.h"
 using namespace Sifteo;
 
+// Constants
 #define NUM_CUBES 		3
 #define LOAD_ASSETS		1
 #define SFX_ON 			0
 #define MUSIC_ON 		0
-#define NUM_ICONS 		6
+#define NUM_ICONS 		4
+#define NUM_TIPS		3
 
+//typedef VidMode_BG0 Canvas;
 typedef VidMode_BG0 Canvas;
 
+// Static Globals
 static Cube gCubes[NUM_CUBES];
-static const AssetImage* gIcons[NUM_ICONS] = {
-	&Icon, &IconGreen, &IconBlue, &Icon, &IconGreen, &IconBlue
-};
+static const AssetImage* gIcons[NUM_ICONS] = { &IconChroma, &IconSandwich, &IconPeano, &IconBuddy };
+static const AssetImage* gTips[NUM_TIPS] = { &Tip0, &Tip1, &Tip2 };
+static const AssetImage* gLabels[NUM_ICONS] = { &LabelChroma, &LabelSandwich, &LabelPeano, &LabelBuddy };
 
 // Modulo operator that always returns an unsigned result
-inline unsigned UnsignedMod(int x, unsigned y) {
-    const int z = x % (int)y;
-    return z < 0 ? z+y : z;
-}
-
-inline float Lerp(float min, float max, float u) {
-	return min + u * (max - min);
-}
-
-inline int ComputeSelected(float u) {
-	int s = (u + (48.f))/96.f;
-	return clamp(s, 0, NUM_ICONS-1);
-}
-
-inline float StoppingPositionFor(int selected) {
-	return 96.f * (selected);
-}
+inline unsigned UnsignedMod(int x, unsigned y) { const int z = x % (int)y; return z < 0 ? z+y : z; }
+inline float Lerp(float min, float max, float u) { return min + u * (max - min); }
+inline int ComputeSelected(float u) { int s = (u + (48.f))/96.f; return clamp(s, 0, NUM_ICONS-1); }
+inline float StoppingPositionFor(int selected) { return 96.f * (selected); }
 
 // positions are in pixel units
 // columns are in tile-units
@@ -48,6 +39,7 @@ static void DrawColumn(Cube* pCube, int x) {
 		// drawing an icon column
 		const AssetImage* pImg = gIcons[x / 12];
 		const uint16_t *src = pImg->tiles + UnsignedMod(local_x, pImg->width);
+		addr += 2*18;
 		for(int row=0; row<10; ++row) {
 	        _SYS_vbuf_writei(&pCube->vbuf.sys, addr, src, 0, 1);
     	    addr += 18;
@@ -58,12 +50,43 @@ static void DrawColumn(Cube* pCube, int x) {
 		// drawing a blank column
 		Canvas g(pCube->vbuf);
 		for(int row=0; row<10; ++row) {
-			g.BG0_drawAsset(Vec2(addr, row), BgTile);
+			g.BG0_drawAsset(Vec2(addr, row+2), BgTile);
 		}
 
 	}
 }
 
+// wrapper for paint()
+static int gCurrentTip = 0;
+static float gPrevTime;
+static void Paint(Cube *pCube) {
+	float time = System::clock();
+	float dt = time - gPrevTime;
+	if (dt > 4.f) {
+		gPrevTime = time - fmodf(dt, 4.f);
+		gCurrentTip = (gCurrentTip+1) % NUM_TIPS;
+		const AssetImage& tip = *gTips[gCurrentTip];
+        _SYS_vbuf_writei(
+        	&pCube->vbuf.sys, 
+        	offsetof(_SYSVideoRAM, bg1_tiles) / 2 + LabelEmpty.width * LabelEmpty.height,
+            tip.tiles, 
+            0, 
+            tip.width * tip.height
+        );
+	}
+	System::paint();
+}
+
+const float kAccelThreshold = 0.85f;
+static float GetAccel(Cube *pCube) {
+	float accel = 0.25f * pCube->virtualAccel().x;
+	#if SIFTEO_SIMULATOR
+		accel = -accel;
+	#endif
+	return accel;
+}
+
+// entry point
 void siftmain() {
 	// enable cube slots
 	for (Cube *p = gCubes; p!=gCubes+NUM_CUBES; ++p) { p->enable(p-gCubes); }
@@ -105,53 +128,80 @@ void siftmain() {
 	System::paintSync();
 	// initialize view
 	Cube *pCube = gCubes;
+	_SYS_vbuf_pokeb(&pCube->vbuf.sys, offsetof(_SYSVideoRAM, mode), _SYS_VM_BG0_BG1);
 	Canvas canvas(pCube->vbuf);
-	canvas.setWindow(16,80);
+    // Allocate tiles for the static upper label, and draw it.
+    {
+    	const AssetImage& label = *gLabels[0];
+    	_SYS_vbuf_fill(&pCube->vbuf.sys, offsetof(_SYSVideoRAM, bg1_bitmap) / 2, ((1 << label.width) - 1), label.height);
+    	_SYS_vbuf_writei(&pCube->vbuf.sys, offsetof(_SYSVideoRAM, bg1_tiles) / 2, label.tiles, 0, label.width * label.height);
+	}
+	// Allocate tiles for the footer, and draw it.
+    _SYS_vbuf_fill(&pCube->vbuf.sys, offsetof(_SYSVideoRAM, bg1_bitmap) / 2 + 12, ((1 << Tip0.width) - 1), Tip0.height);
+    _SYS_vbuf_writei(
+    	&pCube->vbuf.sys, 
+    	offsetof(_SYSVideoRAM, bg1_tiles) / 2 + LabelEmpty.width * LabelEmpty.height,
+        Tip0.tiles, 
+        0, 
+        Tip0.width * Tip0.height
+    );
     for (int x = -1; x < 17; x++) { DrawColumn(pCube, x); }
+    Paint(pCube);
     // initialize physics
 	float velocity = 0;
     float position = 0;
 	int prev_ut = 0;
-
-
+	gPrevTime = System::clock();
+	// wait for a tilt
 	for(;;) {
-		// update physics
-		float accel = 0.25f * pCube->virtualAccel().x;
-		#if SIFTEO_SIMULATOR
-			accel = -accel;
-		#endif
-		const float accelThreshold = 1.25f;
-		const float dt = 0.225f;
-		const bool isTilting = fabs(accel) > accelThreshold;
-		const bool isLefty = position < 0.f - 0.05f;
-		const bool isRighty = position > 96.f*(NUM_ICONS-1) + 0.05f;
-		if (isTilting && !isLefty && !isRighty) {
-			velocity += accel * dt;
-			velocity *= 0.95f;
-		} else {
-			//if (isLefty && velocity < 0.f) { velocity = 0.f; }
-			//if (isRighty && velocity > 0.f) { velocity = 0.f; }
-			const float stiffness = 0.2f;
-			const int selected = ComputeSelected(position);
-			const float stopping_position = StoppingPositionFor(selected);
-			velocity += stiffness * (stopping_position - position) * dt;
-			velocity *= 0.825f;
+		while(fabs(GetAccel(pCube)) < kAccelThreshold) {
+			Paint(pCube);
 		}
-		position += velocity * dt;
-		const float pad = 24.f;
-		//position = clamp(position, -pad, 96.f*(NUM_ICONS-1)+pad);
-		// update view
-		int ui = position + 0.5f;
-		int ut = position / 8;
-		while(prev_ut < ut) {
-			DrawColumn(pCube, prev_ut + 17);
-			prev_ut++;
+	    _SYS_vbuf_writei(&pCube->vbuf.sys, offsetof(_SYSVideoRAM, bg1_tiles) / 2, LabelEmpty.tiles, 0, LabelEmpty.width * LabelEmpty.height);
+		bool doneTilting = false;
+		while(!doneTilting) {
+			// update physics
+			const float accel = GetAccel(pCube);
+			const float dt = 0.225f;
+			const bool isTilting = fabs(accel) > kAccelThreshold;
+			const bool isLefty = position < 0.f - 0.05f;
+			const bool isRighty = position > 96.f*(NUM_ICONS-1) + 0.05f;
+			if (isTilting && !isLefty && !isRighty) {
+				velocity += accel * dt;
+				velocity *= 0.95f;
+			} else {
+				const float stiffness = 0.2f;
+				const int selected = ComputeSelected(position);
+				const float stopping_position = StoppingPositionFor(selected);
+				velocity += stiffness * (stopping_position - position) * dt;
+				velocity *= 0.875f;
+				doneTilting = fabs(stopping_position - position) < 0.5f;
+			}
+			position += velocity * dt;
+			const float pad = 24.f;
+			//position = clamp(position, -pad, 96.f*(NUM_ICONS-1)+pad);
+			// update view
+			int ui = position + 0.5f;
+			int ut = position / 8;
+			while(prev_ut < ut) {
+				DrawColumn(pCube, prev_ut + 17);
+				prev_ut++;
+			}
+			while(prev_ut > ut) {
+				DrawColumn(pCube, prev_ut - 2);
+				prev_ut--;
+			}
+			canvas.BG0_setPanning(Vec2(ui, 0));
+			Paint(pCube);
 		}
-		while(prev_ut > ut) {
-			DrawColumn(pCube, prev_ut - 2);
-			prev_ut--;
+		{
+			const AssetImage& label = *gLabels[ComputeSelected(position)];
+		    _SYS_vbuf_writei(&pCube->vbuf.sys, offsetof(_SYSVideoRAM, bg1_tiles) / 2, label.tiles, 0, label.width * label.height);
 		}
-		canvas.BG0_setPanning(Vec2(ui, 0));
-		System::paint();
 	}
+
+//	canvas.setWindow(16,80);
+//	for (int x = prev_ut; x < prev_ut+18; x++) { DrawColumn(pCube, x); }
+
+	for(;;) { System::paint(); }
 }
