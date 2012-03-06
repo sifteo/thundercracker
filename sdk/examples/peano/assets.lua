d59d70fc8ea3a88424afa711d2cb6f1d113a0a30
Kevin Grandt <kevgrandt@gmail.com>
2012-03-05 22:38:14 -0800

    kill the music assets

--- sdk/examples/peano/assets.lua
+++ sdk/examples/peano/assets.lua
@@ -156,31 +156,27 @@ MinorSE = image{"assets_minorSE.png", width=8, height=8}
 MinorNE = image{"assets_minorNE.png", width=8, height=8}
 
 
-sfx_Stinger_02 = sound{ "PV_Stinger_02.raw" }
-sfx_Slide_LessScrape_01 = sound{ "PV_slide_lessScrape_01.raw" }
-sfx_Slide_LessScrape_02 = sound{ "PV_slide_lessScrape_02.raw" }
-sfx_Slide_LessScrape_Close_01 = sound{ "PV_slide_lessScrape_close_01.raw" }
-sfx_PeanosVaultCrimeWave = sound { "PeanosVaultCrimeWave.raw" }
-sfx_PeanosVaultInsideJob = sound { "PeanosVaultInsideJob.raw" }
-sfx_PeanosVaultSneakers = sound { "PeanosVaultSneakers.raw" }
-sfx_Hide_Overlay = sound { "PV_hide_overlay.raw" }
-sfx_Hint_Deny_01 = sound { "PV_hint_deny_01.raw" }
-sfx_Hint_Deny_02 = sound { "PV_hint_deny_02.raw" }
-sfx_Hint_Stinger_01 = sound { "PV_hint_stinger_01.raw" }
-sfx_Hint_Stinger_02 = sound { "PV_hint_stinger_02.raw" }
-sfx_Target_Overlay = sound { "PV_target_overlay.raw" }
-sfx_Tutorial_Mix_Nums = sound {"PV_tutorial_mix_nums.raw" }
-sfx_Level_Clear = sound {"PV_level_clear.raw" }
-sfx_Connect = sound { "PV_connect.raw" }
-sfx_Fast_Tick = sound{ "PV_fast_tick.raw" }
-sfx_PeanosVaultMenu = sound { "PeanosVaultMenu.raw" }
-sfx_Menu_Tilt_Stop = sound { "PV_menu_tilt_stop.raw" }
-sfx_Menu_Tilt = sound { "PV_menu_tilt.raw" }
-sfx_Tutorial_Correct = sound { "PV_tutorial_correct.raw" }
-sfx_Tutorial_Stinger_01 = sound { "PV_tutorial_stinger_01.raw" }
-sfx_Dialogue_Balloon = sound { "PV_dialogue_balloon.raw" }
-sfx_Tutorial_Oops = sound { "PV_tutorial_oops.raw" }
-sfx_Chapter_Victory = sound { "PV_chapter_victory.raw" }
+sfx_Stinger_02 = sound{ "PV_Stinger_02.raw", encode="PCM" }
+sfx_Slide_LessScrape_01 = sound{ "PV_slide_lessScrape_01.raw", encode="PCM" }
+sfx_Slide_LessScrape_02 = sound{ "PV_slide_lessScrape_02.raw", encode="PCM" }
+sfx_Slide_LessScrape_Close_01 = sound{ "PV_slide_lessScrape_close_01.raw", encode="PCM" }
+sfx_Hide_Overlay = sound { "PV_hide_overlay.raw", encode="PCM" }
+sfx_Hint_Deny_01 = sound { "PV_hint_deny_01.raw", encode="PCM" }
+sfx_Hint_Deny_02 = sound { "PV_hint_deny_02.raw", encode="PCM" }
+sfx_Hint_Stinger_01 = sound { "PV_hint_stinger_01.raw", encode="PCM" }
+sfx_Hint_Stinger_02 = sound { "PV_hint_stinger_02.raw", encode="PCM" }
+sfx_Target_Overlay = sound { "PV_target_overlay.raw", encode="PCM" }
+sfx_Tutorial_Mix_Nums = sound {"PV_tutorial_mix_nums.raw", encode="PCM" }
+sfx_Level_Clear = sound {"PV_level_clear.raw", encode="PCM" }
+sfx_Connect = sound { "PV_connect.raw", encode="PCM" }
+sfx_Fast_Tick = sound{ "PV_fast_tick.raw", encode="PCM" }
+sfx_Menu_Tilt_Stop = sound { "PV_menu_tilt_stop.raw", encode="PCM" }
+sfx_Menu_Tilt = sound { "PV_menu_tilt.raw", encode="PCM" }
+sfx_Tutorial_Correct = sound { "PV_tutorial_correct.raw", encode="PCM" }
+sfx_Tutorial_Stinger_01 = sound { "PV_tutorial_stinger_01.raw", encode="PCM" }
+sfx_Dialogue_Balloon = sound { "PV_dialogue_balloon.raw", encode="PCM" }
+sfx_Tutorial_Oops = sound { "PV_tutorial_oops.raw", encode="PCM" }
+sfx_Chapter_Victory = sound { "PV_chapter_victory.raw", encode="PCM" }
 
 
 
