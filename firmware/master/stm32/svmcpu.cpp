ece425a78f8cf1d71e4c185da3a5e69879ede69e
Liam Staskawicz <liam@sifteo.com>
2012-03-15 12:22:27 -0700

    * more fully fleshed out svc handler - saving and restoring both hw stacked and general regs

--- firmware/master/stm32/svmcpu.cpp
+++ firmware/master/stm32/svmcpu.cpp
@@ -10,21 +10,25 @@
 
 using namespace Svm;
 
-NAKED_HANDLER ISR_SVCall()
-{
-    asm volatile(   "tst    lr, #0x4            \n\t"   // LR bit 2 determines whether we came from user or kernel stack
-                    "ite    eq                  \n\t"   // load r0 with msp or psp based on that
-                    "mrseq  r0, msp             \n\t"
-                    "mrsne  r0, psp             \n\t"
-                    "ldr    r1, [r0, #24]       \n\t"   // load r1 with the stacked PC
-                    "ldrb   r0, [r1, #-2]       \n\t"   // extract the imm8 from the instruction into r0
-                    "bl    %[target]            \n\t"   // and pass it to SvmRuntime::svc
-                    "bx      lr"
-                    : : [target] ""(SvmRuntime::svc));
-}
-
 namespace SvmCpu {
 
+static struct  {
+    IrqContext irq;
+    HwContext hw;
+    reg_t sp;
+} userRegs;
+
+static void saveHwContext(reg_t sp)
+{
+    uint32_t *spp = reinterpret_cast<uint32_t*>(sp);
+    memcpy(&userRegs.hw, spp, sizeof userRegs.hw);
+}
+
+static void restoreHwContext()
+{
+    uint32_t *spp = reinterpret_cast<uint32_t*>(userRegs.sp);
+    memcpy(spp, &userRegs.hw, sizeof userRegs.hw);
+}
 
 /***************************************************************************
  * Public Functions
@@ -37,6 +41,10 @@ void init()
 
 void run(reg_t sp, reg_t pc)
 {
+    asm(
+        "mrs   %[sp_arg], psp          \n\t"
+        "bx    %[target]" : : [sp_arg] "r"(sp), [target] "r"(pc)
+    );
     for (;;) {
         asm volatile ("wfi");
     }
@@ -59,12 +67,84 @@ void run(reg_t sp, reg_t pc)
 
 reg_t reg(uint8_t r)
 {
-    return 0;
+    switch (r) {
+    case 0:         return userRegs.hw.r0;
+    case 1:         return userRegs.hw.r1;
+    case 2:         return userRegs.hw.r2;
+    case 3:         return userRegs.hw.r3;
+    case 4:         return userRegs.irq.r4;
+    case 5:         return userRegs.irq.r5;
+    case 6:         return userRegs.irq.r6;
+    case 7:         return userRegs.irq.r7;
+    case 8:         return userRegs.irq.r8;
+    case 9:         return userRegs.irq.r9;
+    case 10:        return userRegs.irq.r10;
+    case 11:        return userRegs.irq.r11;
+    case 12:        return userRegs.hw.r12;
+    case REG_SP:    return userRegs.sp + sizeof(HwContext);
+    case REG_PC:    return userRegs.hw.returnAddr;
+    default:        return 0;   // TODO: trap this?
+    }
 }
 
 void setReg(uint8_t r, reg_t val)
 {
+    switch (r) {
+    case 0:         userRegs.hw.r0 = val; break;
+    case 1:         userRegs.hw.r1 = val; break;
+    case 2:         userRegs.hw.r2 = val; break;
+    case 3:         userRegs.hw.r3 = val; break;
+    case 4:         userRegs.irq.r4 = val; break;
+    case 5:         userRegs.irq.r5 = val; break;
+    case 6:         userRegs.irq.r6 = val; break;
+    case 7:         userRegs.irq.r7 = val; break;
+    case 8:         userRegs.irq.r8 = val; break;
+    case 9:         userRegs.irq.r9 = val; break;
+    case 10:        userRegs.irq.r10 = val; break;
+    case 11:        userRegs.irq.r11 = val; break;
+    case 12:        userRegs.hw.r12 = val; break;
+    case REG_SP:    userRegs.sp = val - sizeof(HwContext); break;
+    case REG_PC:    userRegs.hw.returnAddr = val; break;
+    // trap default case as invalid?
+    }
 }
 
 } // namespace SvmCpu
+
+/*
+ * SVC handler.
+ * Copy cortex-m3 stacked registers (HwContext) to trusted memory,
+ * and copy regs 4-11 to trusted memory for manipulation by the runtime.
+ *
+ * After runtime handling, the desired user stack pointer may have been modified,
+ * so copy the hardware stacked regs to this location, and update the user sp
+ * before exiting such that HW unstacking finds them at the correct location.
+ */
+NAKED_HANDLER ISR_SVCall()
+{
+    asm volatile(
+        "tst    lr, #0x4            \n\t"   // LR bit 2 determines if regs were stacked to user or main stack
+        "ite    eq                  \n\t"   // load r0 with msp or psp based on that
+        "mrseq  r0, msp             \n\t"
+        "mrsne  r0, psp             \n\t"
+        "bl     %[saveHW]           \n\t"   // save stacked hw regs (sp is in r0) - can optimize to asm when needed
+        "ldr    r2, =%[usrirq]      \n\t"   // load pointer to userRegs.irq into r2
+        "stm    r2, { r4, r5, r6, r7, r8, r9, r10, r11 }    \n\t"   // save copy of user regs
+        "ldr    r3, [r0, #24]       \n\t"   // load r3 with the stacked PC
+        "ldrb   r0, [r3, #-2]       \n\t"   // extract the imm8 from the instruction into r0
+        "bl     %[target]           \n\t"   // and pass sp and imm8 to SvmRuntime::svc
+        "ldr    r2, =%[usrirq]      \n\t"   // load pointer to userRegs.irq into r2
+        "ldm    r2, { r4, r5, r6, r7, r8, r9, r10, r11 }    \n\t"   // restore saved regs
+        "ldr    r3, =%[savedSp]     \n\t"
+        "msr    psp, r3             \n\t"   // copy saved user sp to psp
+        "bl     %[restoreHW]        \n\t"   // copy hw regs to potentially modified user sp
+        "bx     lr"
+        :
+        : [saveHW] "i"(SvmCpu::saveHwContext),
+            [usrirq] "i"(&SvmCpu::userRegs.irq),
+            [target] "i"(SvmRuntime::svc),
+            [restoreHW] "i"(SvmCpu::restoreHwContext),
+            [savedSp] "i"(&SvmCpu::userRegs.sp)
+    );
+}
 
