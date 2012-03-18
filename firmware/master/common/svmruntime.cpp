f59fcd05d26516aa35634be77a729ad4f10ed0f2
M. Elizabeth Scott <beth@sifteo.com>
2012-03-17 22:25:23 -0700

    Fix off-by-one

--- firmware/master/common/svmruntime.cpp
+++ firmware/master/common/svmruntime.cpp
@@ -36,7 +36,7 @@ static void logTitleInfo(Elf::ProgramInfo &pInfo)
     if (uuid) {
         LOG(("SVM: Title UUID is {"));
         for (unsigned i = 0; i < 16; i++)
-            LOG(("%02x%s", uuid->bytes[i], (i == 4 || i == 6 || i == 8 || i == 10) ? "-" : ""));
+            LOG(("%02x%s", uuid->bytes[i], (i == 3 || i == 5 || i == 7 || i == 9) ? "-" : ""));
         LOG(("}\n"));
     }
 #endif
