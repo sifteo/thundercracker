e970093b2dae657a57e8221d9776035a558c0317
Winston <winstonw@stratolab.com>
2012-04-02 17:43:23 -0700

    Just starting on TMNT:Ninja Slide: Michelangelo runs around.

---
+++ sdk/examples/ninja_slide/assets.lua
@@ -0,0 +1,5 @@
+GameAssets = group{quality=10}
+MyBackground = image{"images/background.png", width=8, height=8}
+
+MySprite = image{"images/sprite.png", width=8, height=8, pinned=true}
+Michelangelo = image{"images/Michelangelo.png", width=16, height=16, pinned=true}
---
+++ sdk/examples/ninja_slide/main.cpp
@@ -0,0 +1,83 @@
+/* -*- mode: C; c-basic-offset: 4; intent-tabs-mode: nil -*-
+ *
+ * Sifteo SDK Example.
+ * Copyright <c> 2011 Sifteo, Inc. All rights reserved.
+ */
+
+#include <sifteo.h>
+#include "assets.gen.h"
+
+using namespace Sifteo;
+
+static Cube cube(0);
+enum SpriteIndices {
+    turtle
+};
+
+void init()
+{
+    cube.enable();
+    cube.loadAssets(GameAssets);
+
+    VidMode_BG0_ROM vid(cube.vbuf);
+    vid.init();
+    do {
+        vid.BG0_progressBar(Vec2(0,7), cube.assetProgress(GameAssets, VidMode_BG0::LCD_width) & ~3, 2); 
+        System::paint();
+    } while (!cube.assetDone(GameAssets));
+}
+
+void main()
+{
+    init();
+    
+    VidMode_BG0_SPR_BG1 vid(cube.vbuf);
+    vid.init();
+    vid.clear(MyBackground);
+     
+    vid.setSpriteImage(turtle, Michelangelo);
+    // 1UPs
+//     for (unsigned i = 1; i < _SYS_VRAM_SPRITES; i++) {
+//         vid.resizeSprite(i, Sprite.width*8, Sprite.height*8);
+//         vid.setSpriteImage(i, Sprite.index);
+//     }
+
+//     // Bullet
+//     vid.resizeSprite(0, Bullet.width*8, Bullet.height*8);
+//     vid.setSpriteImage(0, Bullet.index);
+
+//     // BG1 Overlay
+//     _SYS_vbuf_fill(&cube.vbuf.sys, offsetof(_SYSVideoRAM, bg1_bitmap) / 2
+//                    + 16 - Overlay.height,
+//                    ((1 << Overlay.width) - 1), Overlay.height);
+//     _SYS_vbuf_writei(&cube.vbuf.sys, offsetof(_SYSVideoRAM, bg1_tiles) / 2,
+//                      Overlay.tiles, 0, Overlay.width * Overlay.height);
+
+    int x = 0;
+    const Int2 center = { (128 - 16)/2, (128 - 16)/2 };
+
+    while (1) {
+        x = (x+1) % 128;
+        int y = 60;
+        vid.moveSprite(turtle, Vec2(x,y));
+
+
+        // Circle of 1UPs
+//         for (unsigned i = 1; i < _SYS_VRAM_SPRITES; i++) {
+            
+//             float angle = frame * 0.075f + (i-1) * (M_PI*2 / (_SYS_VRAM_SPRITES-1));
+//             const float r = 32;
+//             const Float2 center = { (128 - 16)/2, (128 - 16)/2 };
+
+//             vid.moveSprite(i, Int2(center + polar(angle, r)).round());
+//         }
+
+//         // Scroll BG1
+//         vid.BG1_setPanning(Vec2(-frame, 0u));
+        
+//         // Flying bullet
+//         vid.moveSprite(0, 130-frame*3, 190-frame);
+
+        System::paint();
+    }
+}
