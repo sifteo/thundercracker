2ba45feea689ed1877cca043275e3827745c7148
M. Elizabeth Scott <beth@sifteo.com>
2012-04-06 12:08:23 -0700

    Image stubs

--- firmware/master/common/syscall_image.cpp
+++ firmware/master/common/syscall_image.cpp
@@ -16,45 +16,59 @@ extern "C" {
 void _SYS_image_memDraw(uint16_t *dest, const _SYSAssetImage *im,
     unsigned dest_stride, unsigned frame)
 {
+    LOG(("Unimplemented! %s\n", __FUNCTION__));
 }
 
 void _SYS_image_memDrawRect(uint16_t *dest, const _SYSAssetImage *im,
     unsigned dest_stride, unsigned frame,
     struct _SYSInt2 *srcXY, struct _SYSInt2 *size)
 {
+    LOG(("Unimplemented! %s\n", __FUNCTION__));
 }
 
 void _SYS_image_BG0Draw(struct _SYSAttachedVideoBuffer *vbuf,
     const _SYSAssetImage *im, uint16_t addr, unsigned frame)
 {
+    _SYSAssetImage header;
+    if (!SvmMemory::copyROData(header, im))
+        return;
+
+    _SYS_vbuf_wrect(&vbuf->vbuf, addr,
+        (const uint16_t*)(im+1) + (header.width * header.height * frame),
+        0, header.width, header.height, header.width, 18);
 }
 
 void _SYS_image_BG0DrawRect(struct _SYSAttachedVideoBuffer *vbuf,
     const _SYSAssetImage *im, uint16_t addr, unsigned frame,
         struct _SYSInt2 *srcXY, struct _SYSInt2 *size)
 {
+    LOG(("Unimplemented! %s\n", __FUNCTION__));
 }
 
 void _SYS_image_BG1Draw(struct _SYSAttachedVideoBuffer *vbuf,
     const _SYSAssetImage *im, struct _SYSInt2 *destXY, unsigned frame)
 {
+    LOG(("Unimplemented! %s\n", __FUNCTION__));
 }
 
 void _SYS_image_BG1DrawRect(struct _SYSAttachedVideoBuffer *vbuf,
     const _SYSAssetImage *im, struct _SYSInt2 *destXY, unsigned frame,
         struct _SYSInt2 *srcXY, struct _SYSInt2 *size)
 {
+    LOG(("Unimplemented! %s\n", __FUNCTION__));
 }
 
 void _SYS_image_BG2Draw(struct _SYSAttachedVideoBuffer *vbuf,
     const _SYSAssetImage *im, uint16_t addr, unsigned frame)
 {
+    LOG(("Unimplemented! %s\n", __FUNCTION__));
 }
 
 void _SYS_image_BG2DrawRect(struct _SYSAttachedVideoBuffer *vbuf,
     const _SYSAssetImage *im, uint16_t addr, unsigned frame,
         struct _SYSInt2 *srcXY, struct _SYSInt2 *size)
 {
+    LOG(("Unimplemented! %s\n", __FUNCTION__));
 }
 
 
