5352ba29ec463d2ab4bac6aca8dc28bfb87e4816
Scott Perry <dev@numist.net>
2012-04-06 23:35:59 -0500

    Snapshot support for AudioSampleData And a bunch of comments, cleanup, etc.

--- firmware/master/common/audiosampledata.cpp
+++ firmware/master/common/audiosampledata.cpp
@@ -6,6 +6,35 @@
 #include "audiosampledata.h"
 #include "svmmemory.h"
 
+/* Providing an array interface to sample data is convenient for the audio
+ * channel, but it's a bit misleading because truly random access patterns
+ * can incur massive performance penalties. In general, requests should be
+ * either monotonically increasing or not earlier than one sample from the
+ * newest sample requested from the object. Seeking backwards is efficient
+ * in two cases: 1) seeking to position 0 (see: reset()) and 2) seeking to
+ * the position previously declared as loop_start in init().
+ */
+int16_t AudioSampleData::operator[](uint32_t sampleNum) {
+    ASSERT(mod);
+    ASSERT(sampleNum < numSamples());
+
+    if (sampleNum < oldestSample()) {
+        if (hasSnapshot() && sampleNum > snapshotData.newestSample - kSampleBufSize) {
+            revert();
+        } else {
+            reset();
+        }
+    }
+
+    if (sampleNum > newestSample || newestSample == kNoSamples)
+        decodeToSample(sampleNum);
+
+    ASSERT(sampleNum <= newestSample && sampleNum >= oldestSample());
+
+    return samples[((sampleNum - oldestSample()) + ringPos) % arraysize(samples)];
+}
+
+// Compute the expected number of bytes needed to store a number of samples.
 uint32_t AudioSampleData::bytesForSamples(uint32_t samples) const {
     switch(mod->type) {
         case _SYS_PCM:
@@ -19,6 +48,18 @@ uint32_t AudioSampleData::bytesForSamples(uint32_t samples) const {
     return 0;
 }
 
+// Save the next sample in the ringbuffer and update related instance data.
+void AudioSampleData::writeNextSample(uint16_t sample) {
+    samples[ringPos++] = sample;
+    ringPos %= arraysize(samples);
+    if(newestSample == kNoSamples) newestSample = 0;
+    else newestSample++;
+    // Take a snapshot if necessary.
+    if (!hasSnapshot() && oldestSample() == loopStart && loopStart > 0) {
+        snapshot();
+    }
+}
+
 void AudioSampleData::decodeToSample(uint32_t sampleNum)
 {
     // Decoders are not expected to decode backwards.
@@ -29,13 +70,17 @@ void AudioSampleData::decodeToSample(uint32_t sampleNum)
         SvmMemory::VirtAddr va = mod->pData + bufPos;
         uint32_t bufLen = bytesForSamples(sampleNum - newestSample);
         if (!SvmMemory::mapROData(ref, va, bufLen, pa)) {
-            LOG(("Could not map %p (length %d)!", (void *)va, sampleNum - newestSample));
+            // Fail in as many ways as possible!
+            LOG((LGPFX"Could not map %p (length %d)!\n",
+                 (void *)va, sampleNum - newestSample));
             ASSERT(false);
-            reset();
+            // The best we can now is be quiet and get out of the way.
+            while(newestSample < sampleNum || newestSample == kNoSamples)
+                writeNextSample(0);
             return;
         }
-        uint8_t *bufPtr = pa;
 
+        uint8_t *bufPtr = pa;
         switch(mod->type) {
             case _SYS_PCM:
                 while(bufPtr < pa + bufLen && (newestSample < sampleNum || newestSample == kNoSamples)) {
@@ -50,7 +95,34 @@ void AudioSampleData::decodeToSample(uint32_t sampleNum)
                 }
                 break;
         }
-
         bufPos += bufPtr - pa;
     }
+}
+
+/*
+ *                            _           _       
+ *  ___ _ __   __ _ _ __  ___| |__   ___ | |_ ___ 
+ * / __| '_ \ / _` | '_ \/ __| '_ \ / _ \| __/ __|
+ * \__ \ | | | (_| | |_) \__ \ | | | (_) | |_\__ \
+ * |___/_| |_|\__,_| .__/|___/_| |_|\___/ \__|___/
+ *                 |_|                            
+ */
+void AudioSampleData::snapshot() {
+    memcpy(snapshotData.samples, samples, sizeof(samples));
+    snapshotData.newestSample = newestSample;
+    snapshotData.ringPos = ringPos;
+    snapshotData.bufPos = bufPos;
+    snapshotData.adpcmDec = adpcmDec;
+}
+
+bool AudioSampleData::hasSnapshot() {
+    return snapshotData.newestSample > 0;
+}
+
+void AudioSampleData::revert() {
+    memcpy(samples, snapshotData.samples, sizeof(samples));
+    newestSample = snapshotData.newestSample;
+    ringPos = snapshotData.ringPos;
+    bufPos = snapshotData.bufPos;
+    adpcmDec = snapshotData.adpcmDec;
 }
