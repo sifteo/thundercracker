473b68d63d470d51a2be4a55e0c7b69491e64aed
Liam Staskawicz <liam@sifteo.com>
2012-04-12 16:06:05 -0700

    * FactoryTest: add handlers * successfully setting/getting radio tx power

--- firmware/master/stm32/factorytest.cpp
+++ firmware/master/stm32/factorytest.cpp
@@ -1,36 +1,84 @@
 #include "factorytest.h"
 #include "usart.h"
 #include "board.h"
+#include "macros.h"
+
+#include "radio.h"
 
 uint8_t FactoryTest::commandBuf[MAX_COMMAND_LEN];
 uint8_t FactoryTest::commandLen;
+
+/*
+ * Table of test handlers.
+ * Order must match the Command enum.
+ */
+FactoryTest::TestHandler const FactoryTest::handlers[] = {
+    nrfCommsHandler,
+    flashCommsHandler,
+    flashReadWriteHandler
+};
 
 void FactoryTest::init()
 {
     commandLen = 0;
 }
 
+/*
+ * A new byte has arrived over the UART.
+ * We're assembling a test command that looks like [len, opcode, params...]
+ * If this byte overruns our buffer, reset our count of bytes received.
+ * Otherwise, dispatch to the appropriate handler when a complete message
+ * has been received.
+ */
 void FactoryTest::onUartIsr()
 {
-    uint16_t status = Usart::Dbg.isr(commandBuf + commandLen);
+    uint8_t rxbyte;
+    uint16_t status = Usart::Dbg.isr(&rxbyte);
     if (status & Usart::STATUS_RXED) {
-        commandLen++;
 
-        // msg too long? error
-        if (commandLen >= MAX_COMMAND_LEN) {
+        // avoid overflow - reset
+        if (commandLen >= MAX_COMMAND_LEN)
             commandLen = 0;
-            return;
-        }
 
-        // msg complete? handle it
-        if (commandBuf[COMMAND_LEN_IDX] == commandLen) {
-            handleTestCommand(commandBuf, commandLen);
+        commandBuf[commandLen++] = rxbyte;
+
+        if (commandBuf[LEN_INDEX] == commandLen) {
+            // dispatch to the appropriate handler
+            uint8_t cmd = commandBuf[CMD_INDEX];
+            if (cmd < arraysize(handlers)) {
+                TestHandler handler = handlers[cmd];
+                // arg[0] is always the command byte
+                handler(commandLen - 1, commandBuf + 1);
+            }
+
             commandLen = 0;
         }
     }
 }
 
-void FactoryTest::handleTestCommand(uint8_t *cmd, uint8_t len)
+/**************************************************************************
+ * T E S T  H A N D L E R S
+ *
+ * Each handler is passed a list of arguments, the first of which is always
+ * the command opcode itself.
+ *
+ **************************************************************************/
+
+void FactoryTest::nrfCommsHandler(uint8_t argc, uint8_t *args)
+{
+    Radio::TxPower pwr = static_cast<Radio::TxPower>(args[1]);
+    Radio::setTxPower(pwr);
+
+    uint8_t response[] = { 3, args[0], Radio::txPower() };
+    Usart::Dbg.write(response, sizeof response);
+}
+
+void FactoryTest::flashCommsHandler(uint8_t argc, uint8_t *args)
+{
+
+}
+
+void FactoryTest::flashReadWriteHandler(uint8_t argc, uint8_t *args)
 {
 
 }
