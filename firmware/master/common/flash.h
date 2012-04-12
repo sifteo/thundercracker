d113533e4b323ba757333f38b01eea00b5534447
Liam Staskawicz <liam@sifteo.com>
2012-04-12 16:28:37 -0700

    * support for reading back manufacturer ID info from external flash

--- firmware/master/common/flash.h
+++ firmware/master/common/flash.h
@@ -14,13 +14,24 @@ public:
     static const unsigned SECTOR_SIZE = 1024 * 4;       // smallest erase granularity
     static const unsigned CAPACITY = 1024 * 1024 * 16;  // total storage capacity
 
+    static const uint8_t MACRONIX_MFGR_ID = 0xC2;
+
     static void init();
 
     static void read(uint32_t address, uint8_t *buf, unsigned len);
     static void write(uint32_t address, const uint8_t *buf, unsigned len);
+    static bool writeInProgress();
+
     static void eraseSector(uint32_t address);
     static void chipErase();
-    static bool writeInProgress();
+
+    struct JedecID {
+        uint8_t manufacturerID;
+        uint8_t memoryType;
+        uint8_t memoryDensity;
+    };
+
+    static void readId(JedecID *id);
 
     // sim only
     static void flush();
