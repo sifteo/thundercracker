aebff8d5c8c19f66e77e4dfcc1dc3c3e32d7be55
Kevin Grandt <kevgrandt@gmail.com>
2012-04-11 20:42:04 -0700

    add boostrap assets

--- sdk/examples/peano/main.cpp
+++ sdk/examples/peano/main.cpp
@@ -8,7 +8,8 @@
 using namespace Sifteo;
 
 
-static AssetSlot MainSlot = AssetSlot::allocate();
+static AssetSlot MainSlot = AssetSlot::allocate()
+        .bootstrap(GameAssets);
 
 static Metadata M = Metadata() 
 .title("Peano\'s Vault") 
@@ -30,29 +31,12 @@ namespace TotalsGame
     };
 }
 
-AssetLoader loader;
-
 void main() {
     TotalsGame::TotalsCube *cubes = TotalsGame::Game::cubes;
-        
-    loader.start(GameAssets, 0, (uint32_t)((1<<NUM_CUBES)-1)<<(32-NUM_CUBES));
-
-  for (int i = 0; i < NUM_CUBES; i++) {
+   for (int i = 0; i < NUM_CUBES; i++) {
       cubes[i].sys = i;
-      cubes[i].vid.initMode(BG0_ROM);
-      cubes[i].vid.attach(cubes[i]);
-      cubes[i].vid.bg0rom.text(vec(1,1), "Loading...");
-  }
-#if LOAD_ASSETS
-  while(!loader.isComplete()) {
-    for (int i = 0; i < NUM_CUBES; i++) {
-      cubes[i].vid.bg0rom.hBargraph(vec(0,7), loader.progress(i, 128));
-    }
-    System::paint();
-  }
+   }
 
-#endif
-    
     TotalsGame::Game::Run();
 }
 
