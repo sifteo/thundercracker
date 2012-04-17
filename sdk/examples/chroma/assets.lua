f9761cee1d5e04f48eeb1018149f515c5144ef0d
Eric Liao <eric@sifteo.com>
2012-04-16 17:09:17 -0700

    undo The Flattening

--- sdk/examples/chroma/assets.lua
+++ sdk/examples/chroma/assets.lua
@@ -1,98 +1,98 @@
 GameAssets = group{quality=10}
 --UI
-Font = image{"font-8x16.png", width=8, height=16, flat=true}
-WhiteFont = image{"WhiteFont.png", width=8, height=16, flat=true}
-BannerImg = image{"banner.png", flat=true}
+Font = image{"font-8x16.png", width=8, height=16}
+WhiteFont = image{"WhiteFont.png", width=8, height=16}
+BannerImg = image{"banner.png"}
 --UI
-UI_Touch_Replay = image{"UI_screen_touch_replay.png", flat=true}
-UI_ExitGame = image{"UI_end_game_exit.png", flat=true}
-UI_NCubesCleared = image{"ui_nCubesCleared.png", flat=true}
-UI_BG = image{"UI_universalBG.png", flat=true}
-UI_Highscores = image{"UI_highScore_bg.png", flat=true}
-UI_Highscores_lowscore = image{"UI_highScore_bg_lowScore.png", flat=true}
-UI_Highlight = image{"UI_highlight.png", flat=true}
+UI_Touch_Replay = image{"UI_screen_touch_replay.png"}
+UI_ExitGame = image{"UI_end_game_exit.png"}
+UI_NCubesCleared = image{"ui_nCubesCleared.png"}
+UI_BG = image{"UI_universalBG.png"}
+UI_Highscores = image{"UI_highScore_bg.png"}
+UI_Highscores_lowscore = image{"UI_highScore_bg_lowScore.png"}
+UI_Highlight = image{"UI_highlight.png"}
 
 --points text
-PointFont = image{"points.png", height=48, flat=true}
+PointFont = image{"points.png", height=48}
 BannerPointsWhite = image{"BannerpointsWhite.png", height=16, pinned=true}
 
 --the gems
-Gem0 = image{"Gem0.png", height=32, flat=true}
-Gem1 = image{"Gem1.png", height=32, flat=true}
-Gem2 = image{"Gem2.png", height=32, flat=true}
-Gem3 = image{"Gem3.png", height=32, flat=true}
-Gem4 = image{"Gem4.png", height=32, flat=true}
-Gem5 = image{"Gem5.png", height=32, flat=true}
-Gem6 = image{"Gem6.png", height=32, flat=true}
-Gem7 = image{"Gem7.png", height=32, flat=true}
-GemEmpty = image{"gem_Empty.png", flat=true}
+Gem0 = image{"Gem0.png", height=32}
+Gem1 = image{"Gem1.png", height=32}
+Gem2 = image{"Gem2.png", height=32}
+Gem3 = image{"Gem3.png", height=32}
+Gem4 = image{"Gem4.png", height=32}
+Gem5 = image{"Gem5.png", height=32}
+Gem6 = image{"Gem6.png", height=32}
+Gem7 = image{"Gem7.png", height=32}
+GemEmpty = image{"gem_Empty.png"}
 --fixed gems
-FixedGem0 = image{"fixedGem0.png", height=32, flat=true}
-FixedGem1 = image{"fixedGem1.png", height=32, flat=true}
-FixedGem2 = image{"fixedGem2.png", height=32, flat=true}
-FixedGem3 = image{"fixedGem3.png", height=32, flat=true}
-FixedGem4 = image{"fixedGem4.png", height=32, flat=true}
-FixedGem5 = image{"fixedGem5.png", height=32, flat=true}
-FixedGem6 = image{"fixedGem6.png", height=32, flat=true}
-FixedGem7 = image{"fixedGem7.png", height=32, flat=true}
-FixedExplode0 = image{"fixedExplode0.png", height=32, flat=true}
-FixedExplode1 = image{"fixedExplode1.png", height=32, flat=true}
-FixedExplode2 = image{"fixedExplode2.png", height=32, flat=true}
-FixedExplode3 = image{"fixedExplode3.png", height=32, flat=true}
-FixedExplode4 = image{"fixedExplode4.png", height=32, flat=true}
-FixedExplode5 = image{"fixedExplode5.png", height=32, flat=true}
-FixedExplode6 = image{"fixedExplode6.png", height=32, flat=true}
-FixedExplode7 = image{"fixedExplode7.png", height=32, flat=true}
-FixedGlimmer = image{"FixedGlimmer.png", height=32, flat=true}
---Cover = image{"cover.png", flat=true}
+FixedGem0 = image{"fixedGem0.png", height=32}
+FixedGem1 = image{"fixedGem1.png", height=32}
+FixedGem2 = image{"fixedGem2.png", height=32}
+FixedGem3 = image{"fixedGem3.png", height=32}
+FixedGem4 = image{"fixedGem4.png", height=32}
+FixedGem5 = image{"fixedGem5.png", height=32}
+FixedGem6 = image{"fixedGem6.png", height=32}
+FixedGem7 = image{"fixedGem7.png", height=32}
+FixedExplode0 = image{"fixedExplode0.png", height=32}
+FixedExplode1 = image{"fixedExplode1.png", height=32}
+FixedExplode2 = image{"fixedExplode2.png", height=32}
+FixedExplode3 = image{"fixedExplode3.png", height=32}
+FixedExplode4 = image{"fixedExplode4.png", height=32}
+FixedExplode5 = image{"fixedExplode5.png", height=32}
+FixedExplode6 = image{"fixedExplode6.png", height=32}
+FixedExplode7 = image{"fixedExplode7.png", height=32}
+FixedGlimmer = image{"FixedGlimmer.png", height=32}
+--Cover = image{"cover.png"}
 --special dots
-hyperdot_idle = image{"hyperball_idle.png", height=32, flat=true}
-hyperdot_explode = image{"hyperball_explode.png", height=32, flat=true}
-hyperdot_activation = image{"hyperball_activation.png", height=16, flat=true}
-rockdot = image{"rock.png", height=32, flat=true}
+hyperdot_idle = image{"hyperball_idle.png", height=32}
+hyperdot_explode = image{"hyperball_explode.png", height=32}
+hyperdot_activation = image{"hyperball_activation.png", height=16}
+rockdot = image{"rock.png", height=32}
 rock_explode = image{"rock_explosion.png", height=32, pinned=true}
-rainball_idle = image{"rainball_idle.png", height=32, flat=true}
-rainball_explode = image{"rainball_explode.png", height=32, flat=true}
+rainball_idle = image{"rainball_idle.png", height=32}
+rainball_explode = image{"rainball_explode.png", height=32}
 mult_lightning = image{"mult_lightning.png", height=32, pinned=true}
 mult_numbers = image{"mult_numbers.png", height=16, pinned=true}
 --timer
-timerStem = image{"timerstem.png", height=32, flat=true}
+timerStem = image{"timerstem.png", height=32}
 timerSprite = image{"timerSprite.png", height=32, pinned=true}
 timerLow = image{"timer_5sec.png", height=32, pinned=true}
 --explosions
-ExplodeGem0 = image{"GemExplode0.png", height=32, flat=true}
-ExplodeGem1 = image{"GemExplode1.png", height=32, flat=true}
-ExplodeGem2 = image{"GemExplode2.png", height=32, flat=true}
-ExplodeGem3 = image{"GemExplode3.png", height=32, flat=true}
-ExplodeGem4 = image{"GemExplode4.png", height=32, flat=true}
-ExplodeGem5 = image{"GemExplode5.png", height=32, flat=true}
-ExplodeGem6 = image{"GemExplode6.png", height=32, flat=true}
-ExplodeGem7 = image{"GemExplode7.png", height=32, flat=true}
+ExplodeGem0 = image{"GemExplode0.png", height=32}
+ExplodeGem1 = image{"GemExplode1.png", height=32}
+ExplodeGem2 = image{"GemExplode2.png", height=32}
+ExplodeGem3 = image{"GemExplode3.png", height=32}
+ExplodeGem4 = image{"GemExplode4.png", height=32}
+ExplodeGem5 = image{"GemExplode5.png", height=32}
+ExplodeGem6 = image{"GemExplode6.png", height=32}
+ExplodeGem7 = image{"GemExplode7.png", height=32}
 --idle glimmer
-GlimmerImg = image{"Glimmer.png", height=32, flat=true}
+GlimmerImg = image{"Glimmer.png", height=32}
 
 
 --UI
 --Main menu
-UI_Main_Menu_Blitz = image{"ui_main_menu_rush.png", flat=true}
-UI_Main_Menu_Puzzle = image{"ui_main_menu_puzzle.png", flat=true}
-UI_Main_Menu_Survival = image{"ui_main_menu_survival.png", flat=true}
-UI_Main_Menu_TipsTouch = image{"ui_main_menu_tips_touch.png", flat=true}
-UI_Main_Menu_TipsTilt = image{"ui_main_menu_tips_tilt.png", flat=true}
-UI_Main_Menu_Topbar = image{"ui_main_menu_topbar.png", flat=true}
-UI_Game_Menu_Continue = image{"ui_game_menu_continue.png", flat=true}
-UI_Game_Menu_Restart = image{"UI_screen_touch_restart.png", flat=true}
-UI_Main_Menu_Continue = image{"ui_main_menu_continue.png", flat=true}
-UI_Main_Menu_NewGame = image{"ui_main_menu_new_game.png", flat=true}
-UI_Main_Menu_Back = image{"ui_main_menu_back.png", flat=true}
-UI_Main_Menu_ChapterSelect = image{"ui_chapter_menu_select.png", flat=true}
-UI_Main_Menu_Chapter = image{"ui_main_menu_chapter.png", flat=true}
-UI_Main_Menu_ChapterLock = image{"ui_chapter_menu_lock.png", flat=true}
-UI_Main_Menu_PuzzleLock = image{"ui_puzzle_menu_lock.png", flat=true}
-UI_Main_Menu_PuzzleBack = image{"ui_puzzle_menu_back.png", flat=true}
-UI_Puzzle_Menu_Topbar = image{"ui_puzzle_mode_topbar.png", flat=true}
-UI_ChapterSelect_Topbar = image{"ui_chapter_select_topbar.png", flat=true}
-UI_PuzzleSelect_Menu_Topbar = image{"ui_puzzle_select_topbar.png", flat=true}
+UI_Main_Menu_Blitz = image{"ui_main_menu_rush.png"}
+UI_Main_Menu_Puzzle = image{"ui_main_menu_puzzle.png"}
+UI_Main_Menu_Survival = image{"ui_main_menu_survival.png"}
+UI_Main_Menu_TipsTouch = image{"ui_main_menu_tips_touch.png"}
+UI_Main_Menu_TipsTilt = image{"ui_main_menu_tips_tilt.png"}
+UI_Main_Menu_Topbar = image{"ui_main_menu_topbar.png"}
+UI_Game_Menu_Continue = image{"ui_game_menu_continue.png"}
+UI_Game_Menu_Restart = image{"UI_screen_touch_restart.png"}
+UI_Main_Menu_Continue = image{"ui_main_menu_continue.png"}
+UI_Main_Menu_NewGame = image{"ui_main_menu_new_game.png"}
+UI_Main_Menu_Back = image{"ui_main_menu_back.png"}
+UI_Main_Menu_ChapterSelect = image{"ui_chapter_menu_select.png"}
+UI_Main_Menu_Chapter = image{"ui_main_menu_chapter.png"}
+UI_Main_Menu_ChapterLock = image{"ui_chapter_menu_lock.png"}
+UI_Main_Menu_PuzzleLock = image{"ui_puzzle_menu_lock.png"}
+UI_Main_Menu_PuzzleBack = image{"ui_puzzle_menu_back.png"}
+UI_Puzzle_Menu_Topbar = image{"ui_puzzle_mode_topbar.png"}
+UI_ChapterSelect_Topbar = image{"ui_chapter_select_topbar.png"}
+UI_PuzzleSelect_Menu_Topbar = image{"ui_puzzle_select_topbar.png"}
 --for clearing
 White = image{"White.png", pinned=true}
 
@@ -100,9 +100,9 @@ bubbles1 = image{"bubbles1.png", height=16, pinned=true}
 bubbles2 = image{"bubbles2.png", height=16, pinned=true}
 
 --lumes
-Lumes_Happy = image{"lumes_happy.png", flat=true}
-Lumes_Neutral = image{"lumes_inprogress.png", flat=true}
-Lumes_Sad = image{"lumes_sad.png", flat=true}
+Lumes_Happy = image{"lumes_happy.png"}
+Lumes_Neutral = image{"lumes_inprogress.png"}
+Lumes_Sad = image{"lumes_sad.png"}
 
 --sounds
 astrokraut = sound{"astrokraut.raw"}
