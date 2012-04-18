93d9a44fb913d514688e2d3a57eb0948cdca3084
Liam Staskawicz <liam@sifteo.com>
2012-04-18 16:23:14 -0700

    * better priority for event vector IDs

--- sdk/include/sifteo/abi.h
+++ sdk/include/sifteo/abi.h
@@ -503,16 +503,20 @@ typedef void (*_SYSCubeEvent)(void *context, _SYSCubeID cid);
 typedef void (*_SYSNeighborEvent)(void *context,
     _SYSCubeID c0, _SYSSideID s0, _SYSCubeID c1, _SYSSideID s1);
 
+/*
+ * The order of these vector IDs dictates priority, so choose them accordingly.
+ * For example, it's important to have accel update last, since it's the spammiest.
+ */
 typedef enum {
     _SYS_NEIGHBOR_ADD = 0,
     _SYS_NEIGHBOR_REMOVE,
     _SYS_CUBE_FOUND,
     _SYS_CUBE_LOST,
     _SYS_CUBE_ASSETDONE,
-    _SYS_CUBE_ACCELCHANGE,
     _SYS_CUBE_TOUCH,
     _SYS_CUBE_TILT,
     _SYS_CUBE_SHAKE,
+    _SYS_CUBE_ACCELCHANGE,
 
     _SYS_NUM_VECTORS,   // Must be last
 } _SYSVectorID;
