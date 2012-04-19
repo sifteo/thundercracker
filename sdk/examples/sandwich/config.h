0b26c3eb3402a45758acf96b3fb81dbd5cdea4cb
Max Kaufmann <max.kaufmann@gmail.com>
2012-04-19 14:15:58 -0700

    end to end YES

--- sdk/examples/sandwich/config.h
+++ sdk/examples/sandwich/config.h
@@ -6,6 +6,6 @@
 #define SFX_ON        				1
 #define GFX_ARTIFACT_WORKAROUNDS	1
 #define PLAYTESTING_HACKS			0
-#define FAST_FORWARD				1
+#define FAST_FORWARD				0
 #define DO_ZOOM						0
 #define DO_MENU						0
