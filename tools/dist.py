5e49389ef0c54985376e2f761a31efaeadbd02be
M. Elizabeth Scott <beth@sifteo.com>
2012-04-20 18:42:53 -0700

    Support symlinks in tools/dist.py

--- tools/dist.py
+++ tools/dist.py
@@ -2,6 +2,7 @@
 
 import os, platform
 import shutil, subprocess
+from ZipDir import ZipDir
 
 TC_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 DIST = os.path.join(TC_ROOT, "dist")
@@ -16,16 +17,17 @@ if __name__ == '__main__':
     print "copying files into place....."
     os.chdir(TC_ROOT)
     packagedir = sdkVersion()
+    zipFilename = "%s.zip" % packagedir
     shutil.rmtree(packagedir, ignore_errors = True)
 
     # don'y copy sifteo games - this won't be needed once they're in their own repo
-    shutil.copytree("sdk", packagedir, ignore = shutil.ignore_patterns(
+    shutil.copytree("sdk", packagedir, True, ignore = shutil.ignore_patterns(
         '.git', '.gitignore', '.DS_Store',
         'buddies', 'chroma', 'ninja_slide', 'peano', 'sandwich', 'word'
     ))
 
-    print "creating sdk archive....."
-    shutil.make_archive(packagedir, "zip", packagedir)
+    print "creating sdk archive '%s'....." % zipFilename
+    ZipDir(packagedir, zipFilename)
     shutil.rmtree(packagedir, ignore_errors = True)
 
     print "done."
