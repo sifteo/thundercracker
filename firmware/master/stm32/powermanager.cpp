130d91ef5816b71bf519c62d2e6c15b7b9cbeaca
Liam Staskawicz <liam@sifteo.com>
2012-04-23 11:48:33 -0700

    * enable power sequencing when we detect vbus connect/disconnect * in order to do so, create exti.cpp since some exti ISR vectors serve multiple exti lines - for instance, RF ISR and vbus ISR are in the same vector

---
+++ firmware/master/stm32/powermanager.cpp
@@ -0,0 +1,75 @@
+#include "powermanager.h"
+#include "board.h"
+
+#include "macros.h"
+
+PowerManager::State PowerManager::_state;
+GPIOPin PowerManager::vbus = USB_VBUS_GPIO;
+
+/*
+ * Early initialization that can (and must) run before global ctors have been run.
+ */
+void PowerManager::earlyInit()
+{
+    GPIOPin vcc20 = VCC20_ENABLE_GPIO;
+    vcc20.setControl(GPIOPin::OUT_2MHZ);
+    vcc20.setHigh();
+}
+
+/*
+ * Other initializations that can happen after global ctors have run.
+ */
+void PowerManager::init()
+{
+    _state = PowerManager::Uninitialized;
+
+    vbus.setControl(GPIOPin::IN_FLOAT);
+    vbus.irqInit();
+    vbus.irqSetFallingEdge();
+    vbus.irqSetRisingEdge();
+    vbus.irqEnable();
+
+    vbusIsr();     // set initial state
+}
+
+/*
+ * Called from ISR context when an edge on vbus is detected.
+ */
+void PowerManager::vbusIsr()
+{
+    vbus.irqAcknowledge();
+
+    State s = static_cast<State>(vbus.isHigh());
+    if (s != _state) {
+
+#if (BOARD >= BOARD_TC_MASTER_REV2)
+        /*
+         * The order in which we sequence these enable lines is important,
+         * especially to ensure that the flash doesn't see 3v before its
+         * regulator has been enabled.
+         * - on transition to usb power, enable flash then enable 3v3
+         * - on transition to battery power, disable 3v3 then disable flash
+         */
+        GPIOPin vcc3v3 = VCC33_ENABLE_GPIO;
+        GPIOPin flashRegEnable = FLASH_REG_EN_GPIO;
+
+        switch (s) {
+        case BatteryPwr:
+            vcc3v3.setLow();
+            flashRegEnable.setLow();
+            break;
+
+        case UsbPwr:
+            flashRegEnable.setHigh();
+            vcc3v3.setHigh();
+            break;
+
+        default:
+            break;
+        }
+#endif
+
+        _state = s;
+    }
+}
+
