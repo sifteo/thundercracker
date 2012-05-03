77c3be9b851829b23908dcb25296acf9c887f416
M. Elizabeth Scott <beth@scanlime.org>
2012-05-03 14:19:34 -0700

    Include platform name in the SDK zip file title

--- tools/dist.py
+++ tools/dist.py
@@ -7,9 +7,34 @@ from ZipDir import ZipDir
 TC_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 DIST = os.path.join(TC_ROOT, "dist")
 
+
+def platformBits():
+    if os.name != "posix":
+        return ""
+    machine = os.uname()[4]
+    if machine == "x86_64":
+        return "64"
+    elif machine in ('i386', 'i686'):
+        return "32"
+    else:
+        raise OSError("Unrecognized machine in uname output")
+
+def platformOS():
+    if os.name == "nt":
+        return "windows"
+
+    if os.name == "posix":
+        sysname = os.uname()[0]
+        if sysname == "Linux":
+            return "linux"
+        if sysname == "Darwin":
+            return "mac"
+
+    raise OSError("Unrecognized OS type")
+
 def sdkVersion():
     githash = subprocess.check_output(["git", "describe", "--tags"]).strip()
-    return "sifteo-sdk-%s" % githash
+    return "sifteo-sdk-%s%s-%s" % (platformOS(), platformBits(), githash)
 
 
 if __name__ == '__main__':
@@ -27,10 +52,12 @@ if __name__ == '__main__':
     ]
 
     # Platform-specific
-    if os.name != 'posix':
-        patterns.append('sifteo-sdk-shell.command')
-    if os.name != 'nt':
-        patterns.append('sifteo-sdk-shell.cmd')
+    if platformOS() != "mac":
+        patterns.append("sifteo-sdk-shell.command")
+    if platformOS() != "windows":
+        patterns.append("sifteo-sdk-shell.cmd")
+    if platformOS() != "linux":
+        patterns.append("sifteo-sdk-shell.sh")
 
     shutil.copytree("sdk", packagedir, True, ignore = shutil.ignore_patterns(*patterns))
 
