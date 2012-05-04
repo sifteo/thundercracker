0273cb7ba741fc6e5a7d60f4178e702f9c0ed7d7
M. Elizabeth Scott <beth@sifteo.com>
2012-05-04 12:13:03 -0700

    A little emulated CRC cleanup
    - Make private things actually private (and inlineable)
    - ASSERT that the hardware is init'ed

--- firmware/master/common/crc.h
+++ firmware/master/common/crc.h
@@ -16,15 +16,6 @@ public:
     static void reset();
     static uint32_t get();
     static void add(uint32_t word);
-
-#ifdef SIFTEO_SIMULATOR
-private:
-    static uint32_t crcshift(uint32_t crctab[]);
-    static uint32_t crcWord(uint32_t crctab[], const uint32_t word);
-
-    static uint32_t currentWord;
-    static uint32_t crctable[32];
-#endif
 };
 
 #endif // CRC_H
