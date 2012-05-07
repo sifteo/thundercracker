77fd2ac72a9edc4b36f5073a6024c232ad0f0b24
Liam Staskawicz <liam@sifteo.com>
2012-05-07 13:14:52 -0700

    * cube fw generator script: put cube firmwares in separate folders by date, use max number of cores for build

--- firmware/cube/multi_address_generator.py
+++ firmware/cube/multi_address_generator.py
@@ -7,22 +7,28 @@
 #
 # Jared Wolff - Sifteo Hardware Engineering
 
-import sys
-import os
-import shutil
-import datetime
-import platform
-import subprocess
+import sys, os, platform
+import shutil, datetime
+import subprocess, multiprocessing
 
 date = datetime.date.today().isoformat()
+destination = "cube_firmware_%s" % date
+os.mkdir(destination)
 
 print "\nCube Binary Generator\nCompiling firmare 0x00 to 0x02...\n"
 
-for chan in ["0x02","0x10","0x20"]:
-	
-	for addr in ["0x00","0x01","0x02"]:
-		subprocess.check_call(["make","clean"])
-		myenv = dict(os.environ)
-		myenv["CFLAGS"] = "-DCUBE_ADDR="+addr+" -DCUBE_CHAN="+chan+" -DHWREV=2"
-		subprocess.check_call(["make"], env=myenv)
-		shutil.move("cube.hex", "cube_"+date+"_chan_"+chan+"_addr_"+addr+".hex")
+cores = multiprocessing.cpu_count()
+
+for chan in [0x02, 0x10, 0x20]:
+    for addr in [0x00, 0x01, 0x02]:
+        subprocess.check_call(["make", "clean"])
+
+        myenv = dict(os.environ)
+        hwrev = 2
+        flags = "-DCUBE_ADDR=0x%x -DCUBE_CHAN=0x%x -DHWREV=%d" % (addr, chan, hwrev)
+        myenv["CFLAGS"] = flags
+
+        subprocess.check_call(["make", "-j%d" % (cores)], env=myenv)
+
+        filename = "cube_chan_0x%x_addr_0x%x.hex" % (chan, addr)
+        shutil.move("cube.hex", os.path.join(destination, filename))
