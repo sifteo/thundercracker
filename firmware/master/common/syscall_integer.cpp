3a493023ac611657d2fac00c006219659b7365a1
Scott Perry <dev@numist.net>
2012-05-08 14:30:38 -0700

    __sync_fetch_and_nand does not exist under clang
    but __sync_fetch_and_and still does!

--- firmware/master/common/syscall_integer.cpp
+++ firmware/master/common/syscall_integer.cpp
@@ -31,7 +31,7 @@ uint32_t _SYS_fetch_and_xor_4(uint32_t *p, uint32_t t) {
 
 uint32_t _SYS_fetch_and_nand_4(uint32_t *p, uint32_t t) {
     if (SvmMemory::mapRAM(p, sizeof *p))
-        return __sync_fetch_and_nand(p, t); // this issues warnings on GCC > 4.4, just ignoring for now
+        return ~(__sync_fetch_and_and(p, t)); // this issues warnings on GCC > 4.4, just ignoring for now
     SvmRuntime::fault(F_SYSCALL_ADDRESS);
     return 0;
 }
