4de3b6e56c0d409191d888fdd912afa58f27c0b2
Scott Perry <dev@numist.net>
2012-05-22 21:10:32 -0700

    non-fatal channel collision checking

--- firmware/master/common/xmtrackerplayer.cpp
+++ firmware/master/common/xmtrackerplayer.cpp
@@ -56,8 +56,7 @@ bool XmTrackerPlayer::play(const struct _SYSXMSong *pSong)
      */
     for (unsigned i = 0; i < pSong->nChannels; i++)
         if (AudioMixer::instance.isPlaying(CHANNEL_FOR(i))) {
-            LOG((LGPFX"Warning: Channel %u is busy. Not playing module.\n", CHANNEL_FOR(i)));
-            return false;
+            LOG((LGPFX"Warning: Channel %u is busy. Module may clobber SFX.\n", CHANNEL_FOR(i)));
         }
 
     // Ok, things look (probably) good.
