9a0fe777ccbd7d27ae67a22e24ed395b873611f8
M. Elizabeth Scott <beth@sifteo.com>
2012-06-02 07:40:21 -0700

    Tweak 'scope color scheme again
    Colors looked washed out on my laptop LCD.

--- emulator/resources/scope_fp.glsl
+++ emulator/resources/scope_fp.glsl
@@ -3,10 +3,10 @@ varying vec2 texCoord;
 uniform sampler2D texture;
 
 const float numChannels = 8.0;
-const float focus = 30.0;
+const float focus = 40.0;
 
-const vec4 bg = vec4(0.00, 0.19, 0.39, 0.4);
-const vec4 fg = vec4(1.00, 1.50, 1.00, 1.0);
+const vec4 bg = vec4(0.0, 0.1, 0.2, 0.5);
+const vec4 fg = vec4(1.2, 1.8, 1.2, 1.0);
 
 const vec4 shadow = vec4(0.0, 0.0, 0.0, 1.0);
 
@@ -19,7 +19,7 @@ void main()
     float sample = texture2D(texture, vec2(samplePos, channelNum / numChannels)).r;
     float closeness = pow(1.0 + abs(sample - texCoord.y), -focus);
 
-    float shadowAlpha = (samplePos + texCoord.y) * 0.2;
+    float shadowAlpha = (samplePos + texCoord.y) * 0.3;
 
     gl_FragColor = mix(mix(bg, fg, closeness), shadow, shadowAlpha);
 }
