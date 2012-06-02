05dc6c3b413391da1af4f6ffd7dc6869a2b259b2
M. Elizabeth Scott <beth@sifteo.com>
2012-06-01 19:01:11 -0700

    Inline trivial Crc32 functions on STM32
    This is a tiny wrapper around hardware, so it should save us
    space and time to inline most of the Crc32 implementation.
    To do that, move it into crc.h with an ifdef.

--- firmware/master/stm32/crc.cpp
+++ firmware/master/stm32/crc.cpp
@@ -8,31 +8,6 @@
 #include "board.h"
 
 
-void Crc32::init()
-{
-    RCC.AHBENR |= (1 << 6);
-}
-
-void Crc32::deinit()
-{
-    RCC.AHBENR &= ~(1 << 6);
-}
-
-void Crc32::reset()
-{
-    CRC.CR = (1 << 0);
-}
-
-uint32_t Crc32::get()
-{
-    return CRC.DR;
-}
-
-void Crc32::add(uint32_t word)
-{
-    CRC.DR = word;
-}
-
 void Crc32::addUniqueness()
 {
     const uint32_t* id = reinterpret_cast<const uint32_t*>(Board::UniqueId);
