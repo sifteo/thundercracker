2d8dab9e49f3b740664a952d6dd74709d0f2c74f
M. Elizabeth Scott <beth@sifteo.com>
2012-06-02 14:37:48 -0700

    More filesystem tests
    - Volume::running()
    - Metadata
    - And some string ops why not

--- test/sdk/filesystem/main.cpp
+++ test/sdk/filesystem/main.cpp
@@ -1,31 +1,41 @@
 /*
- * Unit tests for the firmware's flash filesystem.
+ * Unit tests for the flash filesystem implementation and APIs.
  */
 
-#include <sifteo/macros.h>
-#include <sifteo/abi.h>
+#include <sifteo.h>
+using namespace Sifteo;
+
+static Metadata M = Metadata::Metadata()
+    .title("Filesystem Test");
 
 
 void checkTestVolumes()
 {
-    _SYSVolumeHandle volumes[16];
-    unsigned numVolumes = _SYS_fs_listVolumes(0x8765, volumes, arraysize(volumes));
+    Array<Volume, 16> volumes;
+    Volume::list(0x8765, volumes);
 
-    ASSERT(numVolumes >= 1);
-    ASSERT(volumes[0] != 0);
-    ASSERT((volumes[0] & 0xFF) != 0);
+    ASSERT(volumes.count() >= 1);
+    ASSERT(volumes[0].sys != 0);
+    ASSERT((volumes[0].sys & 0xFF) != 0);
 }
 
 void checkSelf()
 {
-    _SYSVolumeHandle volumes[64];
-    unsigned numVolumes = _SYS_fs_listVolumes(_SYS_FS_VOL_GAME, volumes, arraysize(volumes));
+    Array<Volume, 16> games;
+    Volume::list(Volume::T_GAME, games);
 
-    for (unsigned i = 0; i < numVolumes; ++i) {
-        _SYSVolumeHandle vol = volumes[i];
+    Array<Volume, 16> launchers;
+    Volume::list(Volume::T_LAUNCHER, launchers);
 
-        LOG("Visiting volume 0x%08x\n", vol);
-    }
+    ASSERT(games.count() == 0);
+    ASSERT(launchers.count() == 1);
+    ASSERT(launchers[0] == Volume::running());
+
+    // Test our metadata, and some string ops
+    MappedVolume self(Volume::running());
+    String<11> buf;
+    buf << self.title();
+    ASSERT(buf == "Filesystem");
 }
 
 void main()
