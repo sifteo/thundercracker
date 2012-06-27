4736eb5c4b98908a8c6b3e32f5f7d6389e8c818e
M. Elizabeth Scott <beth@sifteo.com>
2012-06-27 02:47:39 -0700

    Another imperceptible difference in Stir's output
    Bump the threshold fairly high up.

--- test/sdk/assetslot/main.cpp
+++ test/sdk/assetslot/main.cpp
@@ -163,7 +163,7 @@ void testMultipleSlots()
         System::finish();
 
         for (CubeID cube : cubes)
-            SCRIPT_FMT(LUA, "util:assertScreenshot(Cube(%d), 'img-%P-frame-%d', 800)",
+            SCRIPT_FMT(LUA, "util:assertScreenshot(Cube(%d), 'img-%P-frame-%d', 10000)",
                 int(cube), image, frame);
     }
 }
