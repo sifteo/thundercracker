7cc2e50c83671a994bb43f46d531707ddfacbc90
M. Elizabeth Scott <beth@sifteo.com>
2012-07-09 07:58:05 -0700

    Tabs

--- firmware/cube/src/sensors_tf0.c
+++ firmware/cube/src/sensors_tf0.c
@@ -152,14 +152,14 @@ void tf0_isr(void) __interrupt(VECTOR_TF0) __naked
         orl     _ack_bits, #RF_ACK_BIT_NEIGHBOR
 
 #ifdef DEBUG_NBR
-        mov		_nbr_temp, a
-        mov 	a, r0
-        clr 	c
-        add		a, #(_nbr_data - RF_ACK_NEIGHBOR)
-        subb	a, #_ack_data
-        mov 	r1, a
-        mov		a, _nbr_temp
-        mov		@r1, a
+        mov     _nbr_temp, a
+        mov     a, r0
+        clr     c
+        add     a, #(_nbr_data - RF_ACK_NEIGHBOR)
+        subb    a, #_ack_data
+        mov     r1, a
+        mov     a, _nbr_temp
+        mov     @r1, a
 #endif
 
 5$:     ; Loop to the next side
