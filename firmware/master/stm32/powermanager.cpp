0b0f1d50fd06b72d9a366d233915c58a96e6d97f
Hakim Raja <huckym@sifteo.com>
2012-07-10 13:54:45 -0700

    comments updated

--- firmware/master/stm32/powermanager.cpp
+++ firmware/master/stm32/powermanager.cpp
@@ -64,20 +64,12 @@ void PowerManager::railTransition(void* p)
     if (SysTime::ticks() < railTransitionDeadline)
         return;
 
-    Tasks::clearPending(Tasks::PowerManager);	//race condition?
+    Tasks::clearPending(Tasks::PowerManager);
 
     State s = static_cast<State>(vbus.isHigh());
     if (s != _state) {
 
 #if (BOARD >= BOARD_TC_MASTER_REV2)
-        /*
-         * The order in which we sequence these enable lines is important,
-         * especially to ensure that the flash doesn't see 3v before its
-         * regulator has been enabled.
-         * - on transition to usb power, enable flash then enable 3v3
-         * - on transition to battery power, disable 3v3 then disable flash
-         */
-
         GPIOPin vcc3v3 = VCC33_ENABLE_GPIO;
 
         switch (s) {
