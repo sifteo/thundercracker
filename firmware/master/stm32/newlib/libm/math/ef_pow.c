0cdb771f6002d7431aefe7125e0ef6ce1630294e
M. Elizabeth Scott <beth@sifteo.com>
2012-07-22 17:08:30 -0700

    Fix some compile warnings in newlib

--- firmware/master/stm32/newlib/libm/math/ef_pow.c
+++ firmware/master/stm32/newlib/libm/math/ef_pow.c
@@ -77,9 +77,14 @@ ivln2_l  =  7.0526075433e-06; /* 0x36eca570 =1/ln2 tail*/
 
     /* x|y==NaN return NaN unless x==1 then return 1 */
 	if(FLT_UWORD_IS_NAN(ix) ||
-	   FLT_UWORD_IS_NAN(iy))
-	    if(ix==0x3f800000) return one;
-	    else return nanf("");
+	   FLT_UWORD_IS_NAN(iy)) {
+	    // SIFTEO: Fixed compiler warning
+	    if(ix==0x3f800000) {
+	        return one;
+	    } else {
+	        return nanf("");
+	    }
+	}
 
     /* determine if y is an odd int when x < 0
      * yisint = 0	... y is not an integer
