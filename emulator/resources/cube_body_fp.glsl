fd505cc964df3aaf7fab1c3cd235ee6b1be20841
M. Elizabeth Scott <beth@sifteo.com>
2012-07-26 21:55:48 -0700

    Fix cube body lighting

--- emulator/resources/cube_body_fp.glsl
+++ emulator/resources/cube_body_fp.glsl
@@ -1,7 +1,10 @@
 varying vec3 normal;
 
-const vec3 lightVec = vec3(1.0, -1.0, 1.0);
-const vec4 diffuse = vec4(0.6, 0.6, 0.6, 1.0);
+// Light direction (somewhat consistent with background lightmap)
+const vec3 lightVec = vec3(-1.0, 2.0, 2.0);
+
+// Light strength/color
+const vec4 diffuse = vec4(0.8, 0.8, 0.8, 1.0);
 const vec4 ambient = vec4(0.2, 0.2, 0.2, 1.0);
 
 void main() {
