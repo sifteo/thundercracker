cbb54eaa131fb089e758861962ba8548355f9380
M. Elizabeth Scott <beth@sifteo.com>
2012-07-30 12:34:58 -0700

    Draw some big 4x5 tile digits for the shutdown timer

--- firmware/master/assets/assets.lua
+++ firmware/master/assets/assets.lua
@@ -5,3 +5,5 @@ IconRestart = image{ "icon-restart.png", flat=1 }
 IconResume = image{ "icon-resume.png", flat=1 }
 
 MenuBackground = image{ "menu-background.png", flat=1 }
+
+BigDigits = image{ "big-digits.png", flat=1, width=32, height=40 }
