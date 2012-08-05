264bde63f11be0d3d9d7ad68f62881ef6b50f296
M. Elizabeth Scott <beth@sifteo.com>
2012-08-05 00:32:32 -0700

    Refactoring: Split up asset.h
    This also separates the (fairly small) non-userspace friendly parts out,
    so that we have less ifdef-hell.

---
+++ sdk/include/sifteo/asset/audio.h
@@ -0,0 +1,83 @@
+/* -*- mode: C; c-basic-offset: 4; intent-tabs-mode: nil -*-
+ *
+ * This file is part of the public interface to the Sifteo SDK.
+ * Copyright <c> 2012 Sifteo, Inc. All rights reserved.
+ */
+
+#pragma once
+#ifdef NOT_USERSPACE
+#   error This is a userspace-only header, not allowed by the current build.
+#endif
+
+#include <sifteo/abi.h>
+
+namespace Sifteo {
+
+/**
+ * @addtogroup assets
+ * @{
+ */
+
+/**
+ * @brief An audio asset, using any supported compression codec.
+ */
+
+struct AssetAudio {
+    _SYSAudioModule sys;
+
+    /**
+     * @brief Return the default speed for this audio asset, in samples per second.
+     *
+     * This can be used to calculate a new speed for AudioChannel::setSpeed().
+     */
+    unsigned speed() {
+        return sys.sampleRate;
+    }
+
+    /**
+     * @brief Create an AssetAudio object programmatically, from uncompressed PCM data
+     *
+     * This creates an AssetAudio instance which references the specified
+     * buffer of 16-bit uncompressed PCM samples. You can use this to wrap
+     * dynamically synthesized audio data for playback on an AudioChannel.
+     *
+     * This is intended mostly for creating software "instruments" for
+     * real-time audio synthesis, so we default to looping mode, and
+     * there is no default sample rate or volume. These parameters
+     * can all be set via AudioChannel at runtime.
+     */
+    static AssetAudio fromPCM(const int16_t *samples, unsigned numSamples)
+    {
+        const AssetAudio result = {{
+            /* sampleRate  */  0,
+            /* loopStart   */  0,
+            /* loopEnd     */  numSamples,
+            /* loopType    */  _SYS_LOOP_REPEAT,
+            /* type        */  _SYS_PCM,
+            /* volume      */  _SYS_AUDIO_DEFAULT_VOLUME,
+            /* dataSize    */  numSamples * sizeof samples[0],
+            /* pData       */  reinterpret_cast<uintptr_t>(samples),
+        }};
+        return result;
+    }
+
+    /// Templatized version of fromPCM(), for fixed-size sample arrays.
+    template <typename T>
+    static AssetAudio fromPCM(const T &samples) {
+        return fromPCM(&samples[0], arraysize(samples));
+    }
+};
+
+/**
+ * @brief A Tracker module, converted from XM format by `stir`
+ */
+
+struct AssetTracker {
+    _SYSXMSong song;
+};
+
+/**
+ * @} end addtogroup assets
+ */
+
+};  // namespace Sifteo
