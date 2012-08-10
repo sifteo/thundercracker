82b2b4ca62ae0b13e7b4a35a5d08e32cdae55750
M. Elizabeth Scott <beth@sifteo.com>
2012-08-10 10:01:33 -0700

    Some bits and pieces that may come in handy tonight

---
+++ extras/drumkit/assets.lua
@@ -0,0 +1,9 @@
+IconGroup = group{quality=9.95}
+Icon = image{"icon.png"}
+
+MyAssets = group{}
+
+Background = image{"background.png"}
+DrumIcon = image{"drum.png"}
+
+DrumSample = sound{"drum.wav"}
