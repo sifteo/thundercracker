8b6e6ee323cb8547c06171b7d4dd2a1f5d95ada7
M. Elizabeth Scott <beth@sifteo.com>
2012-08-13 22:07:35 -0700

    Behavioral model for LIS3DH motion interrupts

--- emulator/src/cube_accel.h
+++ emulator/src/cube_accel.h
@@ -17,6 +17,7 @@
 #define _CUBE_ACCEL_H
 
 #include <stdint.h>
+#include <algorithm>
 
 namespace Cube {
 
@@ -24,10 +25,73 @@ namespace Cube {
 class I2CAccelerometer {
  public:
 
-    void setVector(int16_t x, int16_t y, int16_t z) {
+    void setVector(int16_t x, int16_t y, int16_t z)
+    {
+        /*
+         * Store the latest sample, and update interrupt state.
+         * This would normally be done at the rate configured by the
+         * ODR registers, but we currently get this data at whatever rate
+         * the physics simulation is running at, and we don't resample it.
+         */
+
+        const int kFilterLog2 = 8;
+        const int kHardwareTicksPerSimulatedTick = 4;
+
+        // Store latest samples
         regs.out_x = x;
         regs.out_y = y;
         regs.out_z = z;
+
+        // Cheesy high-pass filter, probably not at all accurate
+        lpX += x - (lpX >> kFilterLog2);
+        lpY += y - (lpY >> kFilterLog2);
+        lpZ += z - (lpZ >> kFilterLog2);
+        if (regs.ctrl_reg[1] & 0x01) {
+            x = std::min(0x7fff, std::max(-0x7fff, int(x) - (lpX >> kFilterLog2)));
+            y = std::min(0x7fff, std::max(-0x7fff, int(y) - (lpY >> kFilterLog2)));
+            z = std::min(0x7fff, std::max(-0x7fff, int(z) - (lpZ >> kFilterLog2)));
+        }
+
+        // Test against high/low interrupt thresholds, create a 6-bit source mask
+        int16_t ths = int(regs.int1_ths) << 8;
+        uint8_t src = 0;
+        if (x < -ths) src |= 0x01; 
+        if (x >  ths) src |= 0x02; 
+        if (y < -ths) src |= 0x04; 
+        if (y >  ths) src |= 0x08; 
+        if (z < -ths) src |= 0x10; 
+        if (z >  ths) src |= 0x20; 
+
+        // Test the bit mask against INT1_CFG, to check our interrupt state.
+        uint8_t cfg = regs.int1_cfg;
+        bool nextInt;
+        switch (cfg & 0xC0) {
+            case 0x00:  // OR combination
+                nextInt = (0 != (cfg & src));
+                break;
+            case 0x40:  // 6-direction edge
+                nextInt = (0 == ((cfg ^ src) & 0x3F) && 0 == intDuration);
+                break;
+            case 0x80:  // AND combination
+                nextInt = ((cfg & src) == (cfg & 0x3F));
+                break;
+            case 0xC0:  // 6-direction position
+                nextInt = (0 == ((cfg ^ src) & 0x3F));
+                break;
+        }
+
+        // Increment/reset interrupt duration
+        if (nextInt)
+            intDuration += kHardwareTicksPerSimulatedTick;
+        else
+            intDuration = 0;
+
+        // Interrupt is active if the test has passed continuously for the set duration
+        // This goes into bit 6 of 'src'
+        if (intDuration > regs.int1_duration)
+            src |= 0x40;
+
+        regs.int1_src = src;
     }
 
     void setADC1(uint16_t value16) {
@@ -98,16 +162,20 @@ class I2CAccelerometer {
 
     bool intPin(unsigned index)
     {
-        // No interrupt sources are currently implemented
-        bool source = false;
+        // Interrupt Active for INT1?
+        ASSERT(index == 1);
+        uint8_t reg6 = regs.ctrl_reg[5];
+        bool source = ((reg6 & regs.int1_src) >> 6) & 1;
         
         // Both interrupts share the same active-high/active-low control pin
-        return source ^ ((regs.ctrl_reg[5] >> 1) & 1);
+        return source ^ ((reg6 >> 1) & 1);
     }
 
  private:
     static const uint8_t deviceAddress = 0x32;
     uint8_t regAddress;
+    unsigned intDuration;
+    int lpX, lpY, lpZ;
 
     // Matches device register layout. Assumes little-endian.
     union {
@@ -131,7 +199,7 @@ class I2CAccelerometer {
             uint8_t fifo_ctrl_reg;      // 2e
             uint8_t fifo_src_reg;       // 2f
             uint8_t int1_cfg;           // 30
-            uint8_t int1_source;        // 31
+            uint8_t int1_src;           // 31
             uint8_t int1_ths;           // 32
             uint8_t int1_duration;      // 33
             uint8_t reserved2[4];       // 34
