88d358604eb070bb1c9257943cdad12ae3d0e9ec
M. Elizabeth Scott <beth@sifteo.com>
2012-08-17 19:50:48 -0700

    New files for UI Asset indirection layer

---
+++ firmware/master/common/ui_assets.cpp
@@ -0,0 +1,54 @@
+/*
+ * Thundercracker Firmware -- Confidential, not for redistribution.
+ * Copyright <c> 2012 Sifteo, Inc. All rights reserved.
+ */
+
+#include "ui_assets.h"
+
+extern const uint16_t v01_LogoWhiteOnBlue_data[];
+extern const uint16_t v01_MenuBackground_data[];
+extern const uint16_t v01_IconQuit_data[];
+extern const uint16_t v01_IconBack_data[];
+extern const uint16_t v01_IconResume_data[];
+extern const uint16_t v01_ShutdownBackground_data[];
+extern const uint16_t v01_BigDigits_data[];
+
+extern const uint16_t v02_LogoWhiteOnBlue_data[];
+extern const uint16_t v02_MenuBackground_data[];
+extern const uint16_t v02_IconQuit_data[];
+extern const uint16_t v02_IconBack_data[];
+extern const uint16_t v02_IconResume_data[];
+extern const uint16_t v02_ShutdownBackground_data[];
+extern const uint16_t v02_BigDigits_data[];
+
+void UIAssets::init(unsigned version)
+{
+    switch (version) {
+
+        case 0x01:
+            logoWhiteOnBlue     = v01_LogoWhiteOnBlue_data;
+            menuBackground      = v01_MenuBackground_data;
+            menuHeight          = 9;
+            iconQuit            = v01_IconQuit_data;
+            iconBack            = v01_IconBack_data;
+            iconResume          = v01_IconResume_data;
+            iconSize            = 5;
+            shutdownBackground  = v01_ShutdownBackground_data;
+            bigDigits           = v01_BigDigits_data;
+            break;
+
+        default:
+        case 0x02:
+            logoWhiteOnBlue     = v02_LogoWhiteOnBlue_data;
+            menuBackground      = v02_MenuBackground_data;
+            menuHeight          = 11;
+            iconQuit            = v02_IconQuit_data;
+            iconBack            = v02_IconBack_data;
+            iconResume          = v02_IconResume_data;
+            iconSize            = 8;
+            shutdownBackground  = v02_ShutdownBackground_data;
+            bigDigits           = v02_BigDigits_data;
+            break;
+
+    }
+}
