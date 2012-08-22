57df4a2b3e9635488bb8f12c342767cf1151e8d1
M. Elizabeth Scott <beth@sifteo.com>
2012-08-22 10:13:09 -0700

    Validator tweaks: Single-pass iteration
    In fact, it's now possible to guarantee that the validator runs in a single pass!

--- firmware/master/common/svmvalidator.cpp
+++ firmware/master/common/svmvalidator.cpp
@@ -17,6 +17,12 @@
 using namespace Svm;
 namespace SvmValidator {
 
+#ifdef SVMVALIDATOR_TRACE
+#   define VALIDATOR_LOG(_x)    LOG(_x)
+#else
+#   define VALIDATOR_LOG(_x)
+#endif
+
 
 // Rotate by 16 bits
 static ALWAYS_INLINE uint32_t rot16(uint32_t word) {
@@ -29,7 +35,7 @@ static ALWAYS_INLINE uint32_t rot16(uint32_t word) {
  * large number if the bundle is totally invalid.
  *
  * The value used for 'INVALID' here doesn't matter at all, so long as it's
- * larger than the number of bundles per block. We pick something that's small
+ * at least as large as the number of bundles per block. We pick something that's small
  * enough to fit in an ARM instruction immediate.
  */
 
@@ -271,92 +277,38 @@ static ALWAYS_INLINE MaxSuccessor decodeBundle(const uint32_t *block, int index)
  * which falls within this set is guaranteed to be valid itself and only
  * transfer control to other valid bundles.
  *
- * This is designed to support inlining all of the above code with a single
- * caller. So, we structure the iteration such that there's no difference
- * between the very first pass and subsequent passes. Each pass is simply
- * a refinement of our current best upper and lower bound for the correct
- * result.
- *
- * This iterative algorithm keeps track of two kinds of state
- *
- *   - An upper bound for the result, which is iteratively refined. This
- *     starts out as the full size of the block, and we decrease it any
- *     time we find an instruction bundle which wouldn't be valid.
- *
- *   - The maximum successor out of all bundles up to the iteration's
- *     upper bound. If this is >= the upper bound, it indicates that
- *     the algorithm has not yet converged and the DAG is not closed.
- *     If it is < the upper bound, we are done and all bundles in the
- *     range are valid.
- *
- * How do we know this algorithm will always converge? At each iteration,
- * we need to know that the upper bound will be decreased. If the algorithm
- * has continued to iterate, that guarantees that there is at least one
- * bundle which now has a successor >= the upper bound. On the next iteration,
- * that bundle will be known to be invalid, causing it to now be excluded
- * from the set of possible results by decreasing the upper bound. Therefore,
- * all iterations of this algorithm are guaranteed to make progress by
- * decreasing the upper bound.
- *
- * An upper bound of zero always indicates we're finished. This means that
- * zero bundles have been examined, and the maximum successor will take on
- * its base-case value of -1.
- *
- * There are a few specific decisions which help this algorithm converge
- * in significantly fewer passes than otherwise:
- *
- *   - We do NOT update our 'maximum successor' estimate for the instruction
- *     which ends up updating upperBound. This lets us complete in one fewer pass
- *     than if we always updated the maximum successor. This is safe because,
- *     at the moment we're processing that bundle, we know it will not be
- *     included in the set of valid results.
- *
- *   - Situations where many valid-looking instructions trail after the
- *     last terminator can be a problem for performance. If we iterated
- *     forward, we would need one full iteration to propagate each "i+1"
- *     successor backwards. It would be bubble-sort, effectively. To
- *     combat this, we can reverse our iteration order.
- *
- * With these tweaks, this algorithm typically runs in one pass.
+ * Everything above gets inlined into this function.
  */
 unsigned findValidBundles(const uint32_t *block)
 {
     MaxSuccessor upperBound = Svm::BUNDLES_PER_BLOCK;
-    MaxSuccessor sMax;
-    DEBUG_ONLY(unsigned numPasses = 0;)
+    MaxSuccessor sMax = -1;
 
-    do {
-        DEBUG_ONLY(numPasses++;)
+    VALIDATOR_LOG(("-------------------- Validating Block --------------------\n"));
 
-        sMax = -1;
+    for (int index = upperBound - 1; index >= 0; --index) {
+        MaxSuccessor sBundle = decodeBundle(block, index);
 
-        for (int index = upperBound - 1; index >= 0; --index) {
-            MaxSuccessor sBundle = decodeBundle(block, index);
+        VALIDATOR_LOG(("[%2d] %04x-%04x %d\n",
+            index, block[index] & 0xffff, block[index] >> 16, sBundle));
 
-            if (sBundle >= upperBound) {
-                /*
-                 * Definitely invalid! The last valid bundle must come before this.
-                 * Reset sMax, since we know we'll need to throw out the results from
-                 * this iteration so far.
-                 */
+        if (sBundle >= upperBound) {
+            /*
+             * Definitely invalid! The last valid bundle must come before this.
+             * Reset sMax, since we know we'll need to throw out the results from
+             * this iteration so far.
+             */
 
-                sMax = -1;
-                upperBound = index;
-            } else {
-                sMax = MAX(sMax, sBundle);
-            }
+            sMax = -1;
+            upperBound = index;
+        } else {
+            sMax = MAX(sMax, sBundle);
         }
 
-    } while (sMax >= upperBound);
+        ASSERT(sMax < upperBound);
+    }
 
-    #ifdef SVMVALIDATOR_TRACE
-        LOG(("-------------------- Validating Block ----- ( result = %d, passes = %d ) \n",
-            upperBound, numPasses));
-        for (unsigned i = 0; i < Svm::BUNDLES_PER_BLOCK; ++i) {
-            LOG(("[%2d] %04x-%04x %d\n", i, block[i] & 0xffff, block[i] >> 16,
-                decodeBundle(block, i)));
-        }
-    #endif
+    VALIDATOR_LOG(("----- Result: %d\n", upperBound));
 
     return upperBound;
 }
