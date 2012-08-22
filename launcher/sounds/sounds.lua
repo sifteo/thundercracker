5debfd88ff32ae341dd7c3ff7b61b7bb613c0ed3
Liam Staskawicz <lstask@gmail.com>
2012-08-22 08:10:03 -0700

    launcher: new audio samples from Dan

--- launcher/sounds/sounds.lua
+++ launcher/sounds/sounds.lua
@@ -1,13 +1,13 @@
 -- Launcher Sounds
 
-Sound_BatteryLowBase = sound{ "sounds/battLowBase.raw" }
-Sound_BatteryLowCube = sound{ "sounds/battLowCube.raw" }
-Sound_ConfirmClick = sound{ "sounds/confirmClick.raw" }
-Sound_NonPossibleAction = sound{ "sounds/nonPossibleAction.raw" }
-Sound_TiltClick = sound{ "sounds/tiltClick.raw" }
-Sound_NoCubesConnected = sound{ "sounds/cubeLost.raw" }
+Sound_BatteryLowBase = sound{ "sounds/UI_battLowBase_2012.08.20.aif_NRML.wav" }
+Sound_BatteryLowCube = sound{ "sounds/UI_battLowCube_2012.08.20.aif_NRML.wav" }
+Sound_ConfirmClick = sound{ "sounds/UI_confirmClick_2012.08.20.aif_NRML.wav" }
+Sound_NonPossibleAction = sound{ "sounds/UI_nonPossibleAction_2012.08.20.aif_NRML.wav" }
+Sound_TiltClick = sound{ "sounds/UI_tiltClick_2012.08.20.aif_NRML.wav" }
+Sound_NoCubesConnected = sound{ "sounds/UI_cubeLost_2012.08.20.aif_NRML.wav" }
 
 Tracker_CubeConnect = tracker{ "sounds/UI_pairedCube_2012.06.14.xm", loop=false }
 Tracker_CubeDisconnect = tracker{ "sounds/UI_unPairedCube_2012.07.05.xm", loop=false }
-Tracker_MenuMusic = tracker{ "sounds/UI_menuMusic04_2012.07.05.xm" }
-Tracker_Startup = tracker{ "sounds/UI_startup_2012.06.14.xm", loop=false }
+Tracker_MenuMusic = tracker{ "sounds/UI_menuMusic04_2012.08.20.xm" }
+Tracker_Startup = tracker{ "sounds/UI_startup_2012.08.20.xm", loop=false }
