7b7399ea233923f6ec2b11b7c8aa4bc5e4b865a5
Scott Perry <github@numist.net>
2012-08-24 16:41:30 -0500

    Play a sound every so often while bootstrapping a game.

--- launcher/sounds/sounds.lua
+++ launcher/sounds/sounds.lua
@@ -6,6 +6,7 @@ Sound_ConfirmClick = sound{ "sounds/confirmClick.raw" }
 Sound_NonPossibleAction = sound{ "sounds/nonPossibleAction.raw" }
 Sound_TiltClick = sound{ "sounds/tiltClick.raw" }
 Sound_NoCubesConnected = sound{ "sounds/cubeLost.raw" }
+Sound_LoadingGame = sound { "sounds/loadingBlip.wav" }
 
 Tracker_CubeConnect = tracker{ "sounds/UI_pairedCube_2012.06.14.xm", loop=false }
 Tracker_CubeDisconnect = tracker{ "sounds/UI_unPairedCube_2012.07.05.xm", loop=false }
