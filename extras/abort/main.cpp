a7bb38687244a04a482cf8ea329a08ae97859953
M. Elizabeth Scott <beth@sifteo.com>
2012-08-24 18:50:32 -0700

    Permissive cube ranges for 'abort' and 'kersplody'

--- extras/abort/main.cpp
+++ extras/abort/main.cpp
@@ -3,7 +3,7 @@
 static Sifteo::Metadata M = Sifteo::Metadata()
     .title("Abort fault test")
     .package("com.sifteo.extras.abort", "1.0")
-    .cubeRange(0);
+    .cubeRange(0, CUBE_ALLOCATION);
 
 void main()
 {
