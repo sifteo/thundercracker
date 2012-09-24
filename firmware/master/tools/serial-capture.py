0994c52de82e43d59961761d33cd3a9fc20fc512
Liam Staskawicz <liam@sifteo.com>
2012-09-24 10:08:19 -0700

    firmware/master/tools: add generic serial capture python script

---
+++ firmware/master/tools/serial-capture.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#
+# simple script to log timestamped messages from a serial connection.
+#
+# would normally just use RealTerm (or similar), but this logs
+# messages to the console as they come in, and can timestamp with
+# millisecond precision, where RealTerm only provides 1 second precision
+#
+
+import sys, serial, datetime
+
+comport = sys.argv[1]
+filepath = sys.argv[2]
+port = serial.Serial(baudrate = 115200, port = comport)
+with open(filepath, "w") as f:
+    while True:
+        line = port.readline().strip()
+        time = str(datetime.datetime.now())
+        log = "%s: %s" % (time, line)
+        print log
+        f.write(log)
+        f.flush()
