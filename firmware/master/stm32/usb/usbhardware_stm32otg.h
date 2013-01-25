e51857122580924f191fa21da8f5a153a697982a
Liam Staskawicz <liam@sifteo.com>
2013-01-24 16:23:51 -0800

    stm32 usb: add status enum

--- firmware/master/stm32/usb/usbhardware_stm32otg.h
+++ firmware/master/stm32/usb/usbhardware_stm32otg.h
@@ -20,6 +20,34 @@ namespace UsbHardwareStm32Otg
         PktStsSetupData     = 6
     };
 
+    enum Status {
+        CMOD            = (1 << 0),     // Current mode of operation
+        MMIS            = (1 << 1),     // Mode mismatch interrupt
+        OTGINT          = (1 << 2),     // OTG interrupt
+        SOF             = (1 << 3),     // Start of frame
+        RXFLVL          = (1 << 4),     // RxFIFO non-empty
+        NPTXFE          = (1 << 5),     // Non-periodic TxFIFO empty
+        GINAKEFF        = (1 << 6),     // Global IN non-periodic NAK effective
+        GONAKEFF        = (1 << 7),     // Global OUT NAK effective
+        ESUSP           = (1 << 10),    // Early suspend
+        USBSUSP         = (1 << 11),    // USB suspend
+        USBRST          = (1 << 12),    // USB reset
+        ENUMDNE         = (1 << 13),    // Enumeration done
+        ISOODRP         = (1 << 14),    // Isochronous OUT packet dropped interrupt
+        EOPF            = (1 << 15),    // End of periodic frame interrupt
+        IEPINT          = (1 << 18),    // IN endpoint interrupt
+        OEPINT          = (1 << 19),    // OUT endpoint interrupt
+        IISOIXFR        = (1 << 20),    // Incomplete isochronous IN transfer
+        INCOMPISOOUT    = (1 << 21),    // Incomplete isochronous OUT transfer
+        HPRTINT         = (1 << 24),    // Host port interrupt
+        HCINT           = (1 << 25),    // Host channels interrupt
+        PTXFE           = (1 << 26),    // Periodic TxFIFO empty
+        CIDSCHG         = (1 << 28),    // Connector ID status change
+        DISCINT         = (1 << 29),    // Disconnect detected interrupt
+        SRQINT          = (1 << 30),    // Session request/new session detected interrupt
+        WKUPINT         = (1 << 31)     // Resume/remote wakeup detected interrupt
+    };
+
     /*
      * In order to tick along the hardware's state machine, we must read packets
      * out of usb ram as soon as they arrive. Stash them here until the application
