d6d82c8a87f3e238c36339b2d0802fe2c91b42eb
Liam Staskawicz <lstask@gmail.com>
2013-02-05 09:59:16 -0800

    compatibility test: update since _SYS_getFeatures() now returns

--- test/sdk/compatibility/main.cpp
+++ test/sdk/compatibility/main.cpp
@@ -8,23 +8,26 @@ using namespace Sifteo;
 void testFeatureBits()
 {
     /*
-     * This is a placeholder for future compatibility bits which can
+     * This is a test for the compatibility bits which can
      * be used to version parts of our ABI. When we add a new group of
      * features, a new bit in this mask will be set.
      *
-     * Currently no such features exist, but we still want to verify
-     * that this syscall is callable without faulting, and that it returns
-     * the expected value of zero.
-     *
      * Note that _SYS_getFeatures() has no direct equivalent in the
-     * documented SDK. The SDK in the future will need to either provide
+     * documented SDK. The SDK must either provide
      * transparent fallback mechanisms for supporting older firmwares, or
-     * they will need to return an appropriate error or export a feature
+     * return an appropriate error or export a feature
      * test predicate such that the game can deal with the error as best
      * it can (for example, by asking the user to upgrade firmware).
      */
 
-    ASSERT(_SYS_getFeatures() == 0);
+    // NOTE: explicitly list each feature bit here to ensure _SYS_getFeatures()
+    // is returning the appropriate value in the event that the feature flags
+    // have been updated
+    ASSERT(_SYS_getFeatures() == _SYS_FEATURE_SYS_VERSION);
+
+    // _SYS_FEATURE_SYS_VERSION - this syscall didn't exist on older OS versions,
+    // so make sure we don't fault when calling it, if it's advertised as available
+    ASSERT(_SYS_version() != _SYS_OS_VERSION_NONE);
 }
 
 void main()
