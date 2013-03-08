f7e8a26eba0368fc7e0a1fa2a1c0b5144f896194
Jared Wolff <jwolff@sifteo.com>
2013-03-08 11:46:20 -0800

    [usbdevice] giving the test jig boot loader a different product ID than the base PID

--- firmware/master/stm32/usb/usbdevice.h
+++ firmware/master/stm32/usb/usbdevice.h
@@ -16,7 +16,9 @@ class UsbDevice
 {
 public:
     static const uint16_t VendorID = 0x22FA;
-#if ((BOARD == BOARD_TEST_JIG) && !defined(BOOTLOADER))
+#if ((BOARD == BOARD_TEST_JIG) && defined(BOOTLOADER))
+    static const uint16_t ProductID = 0x0120;
+#elif ((BOARD == BOARD_TEST_JIG) && !defined(BOOTLOADER))
     static const uint16_t ProductID = 0x0110;
 #elif (defined(BOOTLOADER))
     static const uint16_t ProductID = 0x0115;
