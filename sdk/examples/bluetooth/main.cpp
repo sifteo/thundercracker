e36c294a2898c9158009c6129dc95d75831e8b93
Micah Elizabeth Scott <beth@scanlime.org>
2013-05-15 12:57:06 -0700

    Empty shell for Bluetooth SDK example

---
+++ sdk/examples/bluetooth/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * Sifteo SDK Example.
+ */
+
+#include <sifteo.h>
+#include "assets.gen.h"
+using namespace Sifteo;
+
+static Metadata M = Metadata()
+    .title("Bluetooth SDK Example")
+    .package("com.sifteo.sdk.bluetooth", "1.0")
+    .icon(Icon)
+    .cubeRange(1);
+
+void main()
+{
+}
